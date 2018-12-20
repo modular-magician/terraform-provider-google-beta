4691bd42fa48fcd7b3e68e0dbdaa581286d37a5b
Nathan McKinley <nmckinley@google.com>
2018-12-20 22:17:45 +0000

    Add Appengine's firewall rules.

--- google-beta/import.go
+++ google-beta/import.go
@@ -3,6 +3,7 @@ package google
 import (
 	"fmt"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -25,7 +26,37 @@ func parseImportId(idRegexes []string, d TerraformResourceData, config *Config) 
 			// Starting at index 1, the first match is the full string.
 			for i := 1; i < len(fieldValues); i++ {
 				fieldName := re.SubexpNames()[i]
-				d.Set(fieldName, fieldValues[i])
+				// This part looks confusing.  Because there is no way to know at
+				// this point whether 'fieldName' corresponds to a TypeString or a
+				// TypeInteger in the resource schema, we need to determine
+				// whether to call d.Set() with 'fieldValues[i]', or with an integer
+				// parsed from 'fieldValues[i]'.  Normally, we would be able to just
+				// use a try/catch pattern - try as a string, and if that doesn't
+				// work, try as an integer, and if that doesn't work, return the
+				// error.  Unfortunately, this is not possible here - during tests,
+				// d.Set(...) will panic if there is an error.  So we try d.Get(),
+				// relying on the behavior that an unset field will return the empty
+				// value for the appropriate type.  So if the result is a number, we'll
+				// set it as a number, but if it's a string, we'll set it as a string.
+				val, _ := d.GetOk(fieldName)
+				if _, ok := val.(string); ok {
+					if err = d.Set(fieldName, fieldValues[i]); err != nil {
+						return err
+					}
+				} else if _, ok := val.(int); ok {
+					if atoi, atoiErr := strconv.Atoi(fieldValues[i]); atoiErr == nil {
+						// If the value can be parsed as an integer, we try to set the
+						// value as an integer.
+						if err = d.Set(fieldName, atoi); err != nil {
+							return err
+						}
+					} else {
+						return fmt.Errorf("%s appears to be an integer, but %v cannot be parsed as an int.", fieldValues[i])
+					}
+				} else {
+					return fmt.Errorf(
+						"We cannot handle %s, which currently has value %v, and should be set to %#v, during import.", fieldName, val, fieldValues[i])
+				}
 			}
 
 			// The first id format is applied first and contains all the fields.
