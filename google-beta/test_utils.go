0f1ab495b1d2eec580cb929e6c89d4e08a042804
Nathan McKinley <nmckinley@google.com>
2019-03-04 20:13:08 +0000

    Changes ancillary to supporting the Terraform mapper.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/test_utils.go
+++ google-beta/test_utils.go
@@ -2,12 +2,13 @@ package google
 
 import (
 	"strconv"
+	"reflect"
 )
 
 type ResourceDataMock struct {
-	FieldsInSchema      map[string]interface{}
+	FieldsInSchema		map[string]interface{}
 	FieldsWithHasChange []string
-	id                  string
+	id					string
 }
 
 func (d *ResourceDataMock) HasChange(key string) bool {
@@ -21,7 +22,22 @@ func (d *ResourceDataMock) HasChange(key string) bool {
 	return exists
 }
 
+func (d *ResourceDataMock) Get(key string) interface{} {
+	v, _ := d.GetOk(key)
+	return v
+}
+
 func (d *ResourceDataMock) GetOk(key string) (interface{}, bool) {
+	v, ok := d.GetOkExists(key)
+	if ok && !isEmptyValue(reflect.ValueOf(v)) {
+		return v, true
+	} else {
+		return v, false
+	}
+}
+
+
+func (d *ResourceDataMock) GetOkExists(key string) (interface{}, bool) {
 	for k, v := range d.FieldsInSchema {
 		if key == k {
 			return v, true
