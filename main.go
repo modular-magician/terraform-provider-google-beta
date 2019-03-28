881a3596be2bd4ef49a0fd32c41ed5d48e98a850
Chris Stephens <chrisst@google.com>
2019-03-28 23:51:51 +0000

    Add a few more files from terraform
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/terraform-providers/terraform-provider-google-beta/google-beta"
+	google "github.com/terraform-providers/terraform-provider-google-beta/google-beta"
 )
 
 func main() {
+	"github.com/terraform-providers/terraform-provider-google-beta/google-beta"
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: google.Provider})
 }
