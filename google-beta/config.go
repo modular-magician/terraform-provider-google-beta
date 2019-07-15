3858f662c581d4320c12e9b0477cd25e888bcf44
Chris Stephens <chrisst@google.com>
2019-07-15 18:17:58 +0000

    [blocked] Cloudrun beta implementation on alpha KNative
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/config.go
+++ google-beta/config.go
@@ -72,6 +72,7 @@ type Config struct {
 	BinaryAuthorizationBasePath string
 	ContainerAnalysisBasePath   string
 	SecurityScannerBasePath     string
+	CloudrunBasePath            string
 
 	AccessContextManagerBasePath string
 	CloudSchedulerBasePath       string
