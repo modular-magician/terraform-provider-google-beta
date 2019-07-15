2c7370cdba9af576fe562ef6204058615483df86
Chris Stephens <chrisst@google.com>
2019-07-15 18:22:02 +0000

    [blocked] Cloudrun beta implementation on alpha KNative
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider.go
+++ google-beta/provider.go
@@ -81,6 +81,7 @@ func Provider() terraform.ResourceProvider {
 			BinaryAuthorizationCustomEndpointEntryKey: BinaryAuthorizationCustomEndpointEntry,
 			ContainerAnalysisCustomEndpointEntryKey:   ContainerAnalysisCustomEndpointEntry,
 			SecurityScannerCustomEndpointEntryKey:     SecurityScannerCustomEndpointEntry,
+			CloudrunCustomEndpointEntryKey:            CloudrunCustomEndpointEntry,
 			// end beta-only products
 			AccessContextManagerCustomEndpointEntryKey: AccessContextManagerCustomEndpointEntry,
 			AppEngineCustomEndpointEntryKey:            AppEngineCustomEndpointEntry,
@@ -201,6 +202,7 @@ func ResourceMapWithErrors() (map[string]*schema.Resource, error) {
 		GeneratedContainerAnalysisResourcesMap,
 		GeneratedSecurityScannerResourcesMap,
 		GeneratedHealthcareResourcesMap,
+		GeneratedCloudrunResourcesMap,
 		// end beta-only products
 		GeneratedAccessContextManagerResourcesMap,
 		GeneratedAppEngineResourcesMap,
@@ -383,6 +385,7 @@ func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 	config.BinaryAuthorizationBasePath = d.Get(BinaryAuthorizationCustomEndpointEntryKey).(string)
 	config.ContainerAnalysisBasePath = d.Get(ContainerAnalysisCustomEndpointEntryKey).(string)
 	config.SecurityScannerBasePath = d.Get(SecurityScannerCustomEndpointEntryKey).(string)
+	config.CloudrunBasePath = d.Get(CloudrunCustomEndpointEntryKey).(string)
 
 	config.AccessContextManagerBasePath = d.Get(AccessContextManagerCustomEndpointEntryKey).(string)
 	config.CloudSchedulerBasePath = d.Get(CloudSchedulerCustomEndpointEntryKey).(string)
@@ -445,6 +448,7 @@ func ConfigureBasePaths(c *Config) {
 	c.BinaryAuthorizationBasePath = BinaryAuthorizationDefaultBasePath
 	c.ContainerAnalysisBasePath = ContainerAnalysisDefaultBasePath
 	c.SecurityScannerBasePath = SecurityScannerDefaultBasePath
+	c.CloudrunBasePath = CloudrunDefaultBasePath
 	// end beta-only products
 	c.AccessContextManagerBasePath = AccessContextManagerDefaultBasePath
 	c.AppEngineBasePath = AppEngineDefaultBasePath
