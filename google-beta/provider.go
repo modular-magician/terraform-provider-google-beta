ff3aa017e5a9a62a14ce3ad47d3bfd3dcda0cbb8
Ty Larrabee <tysen@google.com>
2019-08-12 21:31:35 +0000

    Add transfer configs
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider.go
+++ google-beta/provider.go
@@ -100,6 +100,7 @@ func Provider() terraform.ResourceProvider {
 			// Generated Products
 			AccessContextManagerCustomEndpointEntryKey: AccessContextManagerCustomEndpointEntry,
 			AppEngineCustomEndpointEntryKey:            AppEngineCustomEndpointEntry,
+			BigqueryDataTransferCustomEndpointEntryKey: BigqueryDataTransferCustomEndpointEntry,
 			BinaryAuthorizationCustomEndpointEntryKey:  BinaryAuthorizationCustomEndpointEntry,
 			CloudBuildCustomEndpointEntryKey:           CloudBuildCustomEndpointEntry,
 			CloudRunCustomEndpointEntryKey:             CloudRunCustomEndpointEntry,
@@ -220,6 +221,7 @@ func ResourceMapWithErrors() (map[string]*schema.Resource, error) {
 	return mergeResourceMaps(
 		GeneratedAccessContextManagerResourcesMap,
 		GeneratedAppEngineResourcesMap,
+		GeneratedBigqueryDataTransferResourcesMap,
 		GeneratedBinaryAuthorizationResourcesMap,
 		GeneratedCloudBuildResourcesMap,
 		GeneratedCloudRunResourcesMap,
@@ -410,6 +412,7 @@ func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 	// Generated products
 	config.AccessContextManagerBasePath = d.Get(AccessContextManagerCustomEndpointEntryKey).(string)
 	config.AppEngineBasePath = d.Get(AppEngineCustomEndpointEntryKey).(string)
+	config.BigqueryDataTransferBasePath = d.Get(BigqueryDataTransferCustomEndpointEntryKey).(string)
 	config.BinaryAuthorizationBasePath = d.Get(BinaryAuthorizationCustomEndpointEntryKey).(string)
 	config.CloudBuildBasePath = d.Get(CloudBuildCustomEndpointEntryKey).(string)
 	config.CloudRunBasePath = d.Get(CloudRunCustomEndpointEntryKey).(string)
@@ -472,6 +475,7 @@ func ConfigureBasePaths(c *Config) {
 	// Generated Products
 	c.AccessContextManagerBasePath = AccessContextManagerDefaultBasePath
 	c.AppEngineBasePath = AppEngineDefaultBasePath
+	c.BigqueryDataTransferBasePath = BigqueryDataTransferDefaultBasePath
 	c.BinaryAuthorizationBasePath = BinaryAuthorizationDefaultBasePath
 	c.CloudBuildBasePath = CloudBuildDefaultBasePath
 	c.CloudRunBasePath = CloudRunDefaultBasePath
