4f389d3b7ee61412705330adc97d2a3de6ae6c1f
Daniel Gonzalez <d.gonzalez.nothnagel@gmail.com>
2019-08-19 18:36:35 +0000

    Add terraform datasource "google_kms_secret_ciphertext"
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider.go
+++ google-beta/provider.go
@@ -397,6 +397,7 @@ func Provider() terraform.ResourceProvider {
 			"google_iam_policy":                               dataSourceGoogleIamPolicy(),
 			"google_iam_role":                                 dataSourceGoogleIamRole(),
 			"google_kms_secret":                               dataSourceGoogleKmsSecret(),
+			"google_kms_secret_ciphertext":                    dataSourceGoogleKmsSecretCiphertext(),
 			"google_kms_key_ring":                             dataSourceGoogleKmsKeyRing(),
 			"google_kms_crypto_key":                           dataSourceGoogleKmsCryptoKey(),
 			"google_kms_crypto_key_version":                   dataSourceGoogleKmsCryptoKeyVersion(),
@@ -424,9 +425,9 @@ func Provider() terraform.ResourceProvider {
 	}
 }
 
-// Generated resources: 84
+// Generated resources: 85
 // Generated IAM resources: 6
-// Total generated resources: 90
+// Total generated resources: 91
 func ResourceMap() map[string]*schema.Resource {
 	resourceMap, _ := ResourceMapWithErrors()
 	return resourceMap
