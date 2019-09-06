160e9947f4e91859a3186ea8b32ec2aa03fdf1e2
emily <emilyye@google.com>
2019-09-06 16:16:21 +0000

    Autogenerate compute router NAT
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider.go
+++ google-beta/provider.go
@@ -457,9 +457,9 @@ func Provider() terraform.ResourceProvider {
 	return provider
 }
 
-// Generated resources: 92
+// Generated resources: 93
 // Generated IAM resources: 18
-// Total generated resources: 110
+// Total generated resources: 111
 func ResourceMap() map[string]*schema.Resource {
 	resourceMap, _ := ResourceMapWithErrors()
 	return resourceMap
@@ -509,6 +509,7 @@ func ResourceMapWithErrors() (map[string]*schema.Resource, error) {
 			"google_compute_resource_policy":                  resourceComputeResourcePolicy(),
 			"google_compute_route":                            resourceComputeRoute(),
 			"google_compute_router":                           resourceComputeRouter(),
+			"google_compute_router_nat":                       resourceComputeRouterNat(),
 			"google_compute_snapshot":                         resourceComputeSnapshot(),
 			"google_compute_ssl_certificate":                  resourceComputeSslCertificate(),
 			"google_compute_managed_ssl_certificate":          resourceComputeManagedSslCertificate(),
@@ -604,7 +605,6 @@ func ResourceMapWithErrors() (map[string]*schema.Resource, error) {
 			"google_compute_project_metadata_item":         resourceComputeProjectMetadataItem(),
 			"google_compute_region_instance_group_manager": resourceComputeRegionInstanceGroupManager(),
 			"google_compute_router_interface":              resourceComputeRouterInterface(),
-			"google_compute_router_nat":                    resourceComputeRouterNat(),
 			"google_compute_router_peer":                   resourceComputeRouterPeer(),
 			"google_compute_security_policy":               resourceComputeSecurityPolicy(),
 			"google_compute_shared_vpc_host_project":       resourceComputeSharedVpcHostProject(),
