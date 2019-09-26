37d3075050e239c03abb66d6f1d2697e1760683d
Nathan McKinley <nmckinley@google.com>
2019-09-26 00:50:04 +0000

    Update to use new SDK.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_composer_environment_test.go
+++ google-beta/resource_composer_environment_test.go
@@ -9,8 +9,8 @@ import (
 	"time"
 
 	"github.com/hashicorp/go-multierror"
-	"github.com/hashicorp/terraform/helper/acctest"
-	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
 	"google.golang.org/api/storage/v1"
 )
