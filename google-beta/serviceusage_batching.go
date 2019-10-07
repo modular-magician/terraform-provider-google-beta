70defc4e33e7dc172705a151d80194fef303b845
Nathan McKinley <nmckinley@google.com>
2019-10-07 20:29:22 +0000

    Old generated base as of 9805815635f20675fb83434fbfa61de8103af2b9.

--- google-beta/serviceusage_batching.go
+++ google-beta/serviceusage_batching.go
@@ -2,10 +2,8 @@ package google
 
 import (
 	"fmt"
-	"log"
-	"time"
-
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"time"
 )
 
 const (
@@ -15,32 +13,10 @@ const (
 // BatchRequestEnableServices can be used to batch requests to enable services
 // across resource nodes, i.e. to batch creation of several
 // google_project_service(s) resources.
-func BatchRequestEnableServices(services map[string]struct{}, project string, d *schema.ResourceData, config *Config) error {
-	// renamed service create calls are relatively likely to fail, so break out
-	// of the batched call to avoid failing that as well
-	for k := range services {
-		if v, ok := renamedServicesByOldAndNewServiceNames[k]; ok {
-			log.Printf("[DEBUG] found renamed service %s (with alternate name %s)", k, v)
-			delete(services, k)
-			// also remove the other name, so we don't enable it 2x in a row
-			delete(services, v)
-
-			// use a short timeout- failures are likely
-			log.Printf("[DEBUG] attempting user-specified name %s", k)
-			err := enableServiceUsageProjectServices([]string{k}, project, config, 1*time.Minute)
-			if err != nil {
-				log.Printf("[DEBUG] saw error %s. attempting alternate name %v", err, v)
-				err2 := enableServiceUsageProjectServices([]string{v}, project, config, 1*time.Minute)
-				if err2 != nil {
-					return fmt.Errorf("Saw 2 subsequent errors attempting to enable a renamed service: %s / %s", err, err2)
-				}
-			}
-		}
-	}
-
+func BatchRequestEnableServices(services []string, project string, d *schema.ResourceData, config *Config) error {
 	req := &BatchRequest{
 		ResourceName: project,
-		Body:         stringSliceFromGolangSet(services),
+		Body:         services,
 		CombineF:     combineServiceUsageServicesBatches,
 		SendF:        sendBatchFuncEnableServices(config, d.Timeout(schema.TimeoutCreate)),
 		DebugId:      fmt.Sprintf("Enable Project Services %s: %+v", project, services),
