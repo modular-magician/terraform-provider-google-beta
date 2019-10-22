294f7220d66df45892c33a94a194191fb17bb839
megan07 <mbang@hashicorp.com>
2019-10-22 16:41:55 +0000

    Old generated base as of b15f8d6a72e91e821c3028b7b19ea24df1e9d8ec.

--- google-beta/resource_sql_database_instance.go
+++ google-beta/resource_sql_database_instance.go
@@ -118,10 +118,6 @@ func resourceSqlDatabaseInstance() *schema.Resource {
 										// start_time is randomly assigned if not set
 										Computed: true,
 									},
-									"location": {
-										Type:     schema.TypeString,
-										Optional: true,
-									},
 								},
 							},
 						},
@@ -694,7 +690,6 @@ func expandBackupConfiguration(configured []interface{}) *sqladmin.BackupConfigu
 		BinaryLogEnabled: _backupConfiguration["binary_log_enabled"].(bool),
 		Enabled:          _backupConfiguration["enabled"].(bool),
 		StartTime:        _backupConfiguration["start_time"].(string),
-		Location:         _backupConfiguration["location"].(string),
 	}
 }
 
@@ -903,7 +898,6 @@ func flattenBackupConfiguration(backupConfiguration *sqladmin.BackupConfiguratio
 		"binary_log_enabled": backupConfiguration.BinaryLogEnabled,
 		"enabled":            backupConfiguration.Enabled,
 		"start_time":         backupConfiguration.StartTime,
-		"location":           backupConfiguration.Location,
 	}
 
 	return []map[string]interface{}{data}
