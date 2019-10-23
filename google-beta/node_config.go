610a99cbf859c0b48af6b4967f0e11f680737996
Riley Karson <rileykarson@google.com>
2019-10-23 16:40:47 +0000

    Make GKE `taint` GA, add `initial_taint` to GKE, document taint as authoritative.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/node_config.go
+++ google-beta/node_config.go
@@ -171,13 +171,45 @@ var schemaNodeConfig = &schema.Schema{
 				},
 			},
 
+			"initial_taint": {
+				Type:          schema.TypeList,
+				Optional:      true,
+				ForceNew:      true,
+				ConflictsWith: []string{"taint"},
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"key": {
+							Type:     schema.TypeString,
+							Required: true,
+							ForceNew: true,
+						},
+						"value": {
+							Type:     schema.TypeString,
+							Required: true,
+							ForceNew: true,
+						},
+						"effect": {
+							Type:         schema.TypeString,
+							Required:     true,
+							ForceNew:     true,
+							ValidateFunc: validation.StringInSlice([]string{"NO_SCHEDULE", "PREFER_NO_SCHEDULE", "NO_EXECUTE"}, false),
+						},
+					},
+				},
+			},
+
 			"taint": {
-				Type:     schema.TypeList,
-				Optional: true,
-				// Computed=true because GKE Sandbox will automatically add taints to nodes that can/cannot run sandboxed pods.
+				Type: schema.TypeList,
+				// GKE Sandbox and GPUs will add taints out of band so this needs to be O+C
+				Optional:         true,
 				Computed:         true,
 				ForceNew:         true,
 				DiffSuppressFunc: taintDiffSuppress,
+				// TODO(rileykarson): Disable DSF, enable ConfigMode in 3.0.0
+				// Legacy config mode allows setting no taint explicitly.
+				// See https://www.terraform.io/docs/configuration/attr-as-blocks.html
+				// ConfigMode: schema.SchemaConfigModeAttr,
+				ConflictsWith: []string{"initial_taint"},
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"key": {
@@ -339,6 +371,19 @@ func expandNodeConfig(v interface{}) *containerBeta.NodeConfig {
 	}
 
 	if v, ok := nodeConfig["taint"]; ok && len(v.([]interface{})) > 0 {
+		taints := v.([]interface{})
+		nodeTaints := make([]*containerBeta.NodeTaint, 0, len(taints))
+		for _, raw := range taints {
+			data := raw.(map[string]interface{})
+			taint := &containerBeta.NodeTaint{
+				Key:    data["key"].(string),
+				Value:  data["value"].(string),
+				Effect: data["effect"].(string),
+			}
+			nodeTaints = append(nodeTaints, taint)
+		}
+		nc.Taints = nodeTaints
+	} else if v, ok := nodeConfig["initial_taint"]; ok && len(v.([]interface{})) > 0 {
 		taints := v.([]interface{})
 		nodeTaints := make([]*containerBeta.NodeTaint, 0, len(taints))
 		for _, raw := range taints {
