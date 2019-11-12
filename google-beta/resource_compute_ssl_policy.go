bc0d86e7ec15e0b106c9fb82c1cd902729e81397
Sam Levenick <slevenick@google.com>
2019-11-12 19:15:34 +0000

    Add upgrade guide for subnetwork log_config

--- google-beta/resource_compute_ssl_policy.go
+++ google-beta/resource_compute_ssl_policy.go
@@ -70,73 +70,39 @@ func resourceComputeSslPolicy() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 				ForceNew: true,
-				Description: `Name of the resource. Provided by the client when the resource is
-created. The name must be 1-63 characters long, and comply with
-RFC1035. Specifically, the name must be 1-63 characters long and match
-the regular expression '[a-z]([-a-z0-9]*[a-z0-9])?' which means the
-first character must be a lowercase letter, and all following
-characters must be a dash, lowercase letter, or digit, except the last
-character, which cannot be a dash.`,
 			},
 			"custom_features": {
 				Type:     schema.TypeSet,
 				Optional: true,
-				Description: `Profile specifies the set of SSL features that can be used by the
-load balancer when negotiating SSL with clients. This can be one of
-'COMPATIBLE', 'MODERN', 'RESTRICTED', or 'CUSTOM'. If using 'CUSTOM',
-the set of SSL features to enable must be specified in the
-'customFeatures' field.
-
-See the [official documentation](https://cloud.google.com/compute/docs/load-balancing/ssl-policies#profilefeaturesupport)
-for which ciphers are available to use. **Note**: this argument
-*must* be present when using the 'CUSTOM' profile. This argument
-*must not* be present when using any other profile.`,
 				Elem: &schema.Schema{
 					Type: schema.TypeString,
 				},
 				Set: schema.HashString,
 			},
 			"description": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				ForceNew:    true,
-				Description: `An optional description of this resource.`,
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
 			},
 			"min_tls_version": {
 				Type:         schema.TypeString,
 				Optional:     true,
 				ValidateFunc: validation.StringInSlice([]string{"TLS_1_0", "TLS_1_1", "TLS_1_2", ""}, false),
-				Description: `The minimum version of SSL protocol that can be used by the clients
-to establish a connection with the load balancer. This can be one of
-'TLS_1_0', 'TLS_1_1', 'TLS_1_2'.
- Default is 'TLS_1_0'.`,
-				Default: "TLS_1_0",
+				Default:      "TLS_1_0",
 			},
 			"profile": {
 				Type:         schema.TypeString,
 				Optional:     true,
 				ValidateFunc: validation.StringInSlice([]string{"COMPATIBLE", "MODERN", "RESTRICTED", "CUSTOM", ""}, false),
-				Description: `Profile specifies the set of SSL features that can be used by the
-load balancer when negotiating SSL with clients. This can be one of
-'COMPATIBLE', 'MODERN', 'RESTRICTED', or 'CUSTOM'. If using 'CUSTOM',
-the set of SSL features to enable must be specified in the
-'customFeatures' field.
-
-See the [official documentation](https://cloud.google.com/compute/docs/load-balancing/ssl-policies#profilefeaturesupport)
-for information on what cipher suites each profile provides. If
-'CUSTOM' is used, the 'custom_features' attribute **must be set**.
-Default is 'COMPATIBLE'.`,
-				Default: "COMPATIBLE",
+				Default:      "COMPATIBLE",
 			},
 			"creation_timestamp": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: `Creation timestamp in RFC3339 text format.`,
+				Type:     schema.TypeString,
+				Computed: true,
 			},
 			"enabled_features": {
-				Type:        schema.TypeSet,
-				Computed:    true,
-				Description: `The list of features enabled in the SSL policy.`,
+				Type:     schema.TypeSet,
+				Computed: true,
 				Elem: &schema.Schema{
 					Type: schema.TypeString,
 				},
@@ -145,8 +111,6 @@ Default is 'COMPATIBLE'.`,
 			"fingerprint": {
 				Type:     schema.TypeString,
 				Computed: true,
-				Description: `Fingerprint of this resource. A hash of the contents stored in this
-object. This field is used in optimistic locking.`,
 			},
 			"project": {
 				Type:     schema.TypeString,
@@ -213,7 +177,7 @@ func resourceComputeSslPolicyCreate(d *schema.ResourceData, meta interface{}) er
 	}
 
 	// Store the ID now
-	id, err := replaceVars(d, config, "{{name}}")
+	id, err := replaceVars(d, config, "projects/{{project}}/global/sslPolicies/{{name}}")
 	if err != nil {
 		return fmt.Errorf("Error constructing id: %s", err)
 	}
@@ -404,7 +368,7 @@ func resourceComputeSslPolicyImport(d *schema.ResourceData, meta interface{}) ([
 	}
 
 	// Replace import id for the resource id
-	id, err := replaceVars(d, config, "{{name}}")
+	id, err := replaceVars(d, config, "projects/{{project}}/global/sslPolicies/{{name}}")
 	if err != nil {
 		return nil, fmt.Errorf("Error constructing id: %s", err)
 	}
