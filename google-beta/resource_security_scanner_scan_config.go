b65ab41c7d4034973856bcefabc5f19f25c851b9
megan07 <mbang@hashicorp.com>
2019-11-12 23:59:59 +0000

    update upgrade guide

--- google-beta/resource_security_scanner_scan_config.go
+++ google-beta/resource_security_scanner_scan_config.go
@@ -45,14 +45,12 @@ func resourceSecurityScannerScanConfig() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"display_name": {
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: `The user provider display name of the ScanConfig.`,
+				Type:     schema.TypeString,
+				Required: true,
 			},
 			"starting_urls": {
-				Type:        schema.TypeList,
-				Required:    true,
-				Description: `The starting URLs from which the scanner finds site pages.`,
+				Type:     schema.TypeList,
+				Required: true,
 				Elem: &schema.Schema{
 					Type: schema.TypeString,
 				},
@@ -60,61 +58,52 @@ func resourceSecurityScannerScanConfig() *schema.Resource {
 			"authentication": {
 				Type:     schema.TypeList,
 				Optional: true,
-				Description: `The authentication configuration.
-If specified, service will use the authentication configuration during scanning.`,
 				MaxItems: 1,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"custom_account": {
-							Type:        schema.TypeList,
-							Optional:    true,
-							Description: `Describes authentication configuration that uses a custom account.`,
-							MaxItems:    1,
+							Type:     schema.TypeList,
+							Optional: true,
+							MaxItems: 1,
 							Elem: &schema.Resource{
 								Schema: map[string]*schema.Schema{
 									"login_url": {
-										Type:        schema.TypeString,
-										Required:    true,
-										Description: `The login form URL of the website.`,
-									},
-									"password": {
 										Type:     schema.TypeString,
 										Required: true,
-										ForceNew: true,
-										Description: `The password of the custom account. The credential is stored encrypted
-in GCP.`,
+									},
+									"password": {
+										Type:      schema.TypeString,
+										Required:  true,
+										ForceNew:  true,
 										Sensitive: true,
 									},
 									"username": {
-										Type:        schema.TypeString,
-										Required:    true,
-										Description: `The user name of the custom account.`,
+										Type:     schema.TypeString,
+										Required: true,
 									},
 								},
 							},
+							AtLeastOneOf: []string{"authentication.0.google_account", "authentication.0.custom_account"},
 						},
 						"google_account": {
-							Type:        schema.TypeList,
-							Optional:    true,
-							Description: `Describes authentication configuration that uses a Google account.`,
-							MaxItems:    1,
+							Type:     schema.TypeList,
+							Optional: true,
+							MaxItems: 1,
 							Elem: &schema.Resource{
 								Schema: map[string]*schema.Schema{
 									"password": {
-										Type:     schema.TypeString,
-										Required: true,
-										ForceNew: true,
-										Description: `The password of the Google account. The credential is stored encrypted
-in GCP.`,
+										Type:      schema.TypeString,
+										Required:  true,
+										ForceNew:  true,
 										Sensitive: true,
 									},
 									"username": {
-										Type:        schema.TypeString,
-										Required:    true,
-										Description: `The user name of the Google account.`,
+										Type:     schema.TypeString,
+										Required: true,
 									},
 								},
 							},
+							AtLeastOneOf: []string{"authentication.0.google_account", "authentication.0.custom_account"},
 						},
 					},
 				},
@@ -122,8 +111,6 @@ in GCP.`,
 			"blacklist_patterns": {
 				Type:     schema.TypeList,
 				Optional: true,
-				Description: `The blacklist URL patterns as described in
-https://cloud.google.com/security-scanner/docs/excluded-urls`,
 				Elem: &schema.Schema{
 					Type: schema.TypeString,
 				},
@@ -132,43 +119,34 @@ https://cloud.google.com/security-scanner/docs/excluded-urls`,
 				Type:         schema.TypeString,
 				Optional:     true,
 				ValidateFunc: validation.StringInSlice([]string{"ENABLED", "DISABLED", ""}, false),
-				Description:  `Controls export of scan configurations and results to Cloud Security Command Center.`,
 				Default:      "ENABLED",
 			},
 			"max_qps": {
 				Type:         schema.TypeInt,
 				Optional:     true,
 				ValidateFunc: validation.IntBetween(5, 20),
-				Description: `The maximum QPS during scanning. A valid value ranges from 5 to 20 inclusively.
-Defaults to 15.`,
-				Default: 15,
+				Default:      15,
 			},
 			"schedule": {
-				Type:        schema.TypeList,
-				Optional:    true,
-				Description: `The schedule of the ScanConfig`,
-				MaxItems:    1,
+				Type:     schema.TypeList,
+				Optional: true,
+				MaxItems: 1,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"interval_duration_days": {
-							Type:        schema.TypeInt,
-							Required:    true,
-							Description: `The duration of time between executions in days`,
+							Type:     schema.TypeInt,
+							Required: true,
 						},
 						"schedule_time": {
 							Type:     schema.TypeString,
 							Optional: true,
-							Description: `A timestamp indicates when the next run will be scheduled. The value is refreshed
-by the server after each run. If unspecified, it will default to current server time,
-which means the scan will be scheduled to start immediately.`,
 						},
 					},
 				},
 			},
 			"target_platforms": {
-				Type:        schema.TypeList,
-				Optional:    true,
-				Description: `Set of Cloud Platforms targeted by the scan. If empty, APP_ENGINE will be used as a default.`,
+				Type:     schema.TypeList,
+				Optional: true,
 				Elem: &schema.Schema{
 					Type: schema.TypeString,
 				},
@@ -177,14 +155,11 @@ which means the scan will be scheduled to start immediately.`,
 				Type:         schema.TypeString,
 				Optional:     true,
 				ValidateFunc: validation.StringInSlice([]string{"USER_AGENT_UNSPECIFIED", "CHROME_LINUX", "CHROME_ANDROID", "SAFARI_IPHONE", ""}, false),
-				Description:  `Type of the user agents used for scanning`,
 				Default:      "CHROME_LINUX",
 			},
 			"name": {
 				Type:     schema.TypeString,
 				Computed: true,
-				Description: `A server defined name for this index. Format:
-'projects/{{project}}/scanConfigs/{{server_generated_id}}'`,
 			},
 			"project": {
 				Type:     schema.TypeString,
