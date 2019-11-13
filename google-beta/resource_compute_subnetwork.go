01d0fc344c0b8efaa55914bf63addba0eef850d7
Paddy <paddy@hashicorp.com>
2019-11-13 05:00:53 +0000

    Password is not actually required.

--- google-beta/resource_compute_subnetwork.go
+++ google-beta/resource_compute_subnetwork.go
@@ -19,7 +19,6 @@ import (
 	"log"
 	"net"
 	"reflect"
-	"strings"
 	"time"
 
 	"github.com/apparentlymart/go-cidr/cidr"
@@ -49,13 +48,6 @@ func isShrinkageIpCidr(old, new, _ interface{}) bool {
 	return true
 }
 
-func splitSubnetID(id string) (region string, name string) {
-	parts := strings.Split(id, "/")
-	region = parts[0]
-	name = parts[1]
-	return
-}
-
 func resourceComputeSubnetwork() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceComputeSubnetworkCreate,
@@ -83,53 +75,27 @@ func resourceComputeSubnetwork() *schema.Resource {
 				Type:         schema.TypeString,
 				Required:     true,
 				ValidateFunc: validateIpCidrRange,
-				Description: `The range of internal addresses that are owned by this subnetwork.
-Provide this property when you create the subnetwork. For example,
-10.0.0.0/8 or 192.168.0.0/16. Ranges must be unique and
-non-overlapping within a network. Only IPv4 is supported.`,
 			},
 			"name": {
 				Type:         schema.TypeString,
 				Required:     true,
 				ForceNew:     true,
 				ValidateFunc: validateGCPName,
-				Description: `The name of the resource, provided by the client when initially
-creating the resource. The name must be 1-63 characters long, and
-comply with RFC1035. Specifically, the name must be 1-63 characters
-long and match the regular expression '[a-z]([-a-z0-9]*[a-z0-9])?' which
-means the first character must be a lowercase letter, and all
-following characters must be a dash, lowercase letter, or digit,
-except the last character, which cannot be a dash.`,
 			},
 			"network": {
 				Type:             schema.TypeString,
 				Required:         true,
 				ForceNew:         true,
 				DiffSuppressFunc: compareSelfLinkOrResourceName,
-				Description: `The network this subnet belongs to.
-Only networks that are in the distributed mode can have subnetworks.`,
 			},
 			"description": {
 				Type:     schema.TypeString,
 				Optional: true,
 				ForceNew: true,
-				Description: `An optional description of this resource. Provide this property when
-you create the resource. This field can be set only at resource
-creation time.`,
-			},
-			"enable_flow_logs": {
-				Type:        schema.TypeBool,
-				Computed:    true,
-				Optional:    true,
-				Deprecated:  "This field is being removed in favor of log_config. If log_config is present, flow logs are enabled.",
-				Description: `Whether to enable flow logging for this subnetwork.`,
 			},
 			"log_config": {
 				Type:     schema.TypeList,
-				Computed: true,
 				Optional: true,
-				Description: `Denotes the logging options for the subnetwork flow logs. If logging is enabled
-logs will be exported to Stackdriver.`,
 				MaxItems: 1,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
@@ -137,32 +103,21 @@ logs will be exported to Stackdriver.`,
 							Type:         schema.TypeString,
 							Optional:     true,
 							ValidateFunc: validation.StringInSlice([]string{"INTERVAL_5_SEC", "INTERVAL_30_SEC", "INTERVAL_1_MIN", "INTERVAL_5_MIN", "INTERVAL_10_MIN", "INTERVAL_15_MIN", ""}, false),
-							Description: `Can only be specified if VPC flow logging for this subnetwork is enabled.
-Toggles the aggregation interval for collecting flow logs. Increasing the
-interval time will reduce the amount of generated flow logs for long
-lasting connections. Default is an interval of 5 seconds per connection.
-Possible values are INTERVAL_5_SEC, INTERVAL_30_SEC, INTERVAL_1_MIN,
-INTERVAL_5_MIN, INTERVAL_10_MIN, INTERVAL_15_MIN`,
-							Default: "INTERVAL_5_SEC",
+							Default:      "INTERVAL_5_SEC",
+							AtLeastOneOf: []string{"log_config.0.aggregation_interval", "log_config.0.flow_sampling", "log_config.0.metadata"},
 						},
 						"flow_sampling": {
-							Type:     schema.TypeFloat,
-							Optional: true,
-							Description: `Can only be specified if VPC flow logging for this subnetwork is enabled.
-The value of the field must be in [0, 1]. Set the sampling rate of VPC
-flow logs within the subnetwork where 1.0 means all collected logs are
-reported and 0.0 means no logs are reported. Default is 0.5 which means
-half of all collected logs are reported.`,
-							Default: 0.5,
+							Type:         schema.TypeFloat,
+							Optional:     true,
+							Default:      0.5,
+							AtLeastOneOf: []string{"log_config.0.aggregation_interval", "log_config.0.flow_sampling", "log_config.0.metadata"},
 						},
 						"metadata": {
 							Type:         schema.TypeString,
 							Optional:     true,
 							ValidateFunc: validation.StringInSlice([]string{"EXCLUDE_ALL_METADATA", "INCLUDE_ALL_METADATA", ""}, false),
-							Description: `Can only be specified if VPC flow logging for this subnetwork is enabled.
-Configures whether metadata fields should be added to the reported VPC
-flow logs. Default is 'INCLUDE_ALL_METADATA'.`,
-							Default: "INCLUDE_ALL_METADATA",
+							Default:      "INCLUDE_ALL_METADATA",
+							AtLeastOneOf: []string{"log_config.0.aggregation_interval", "log_config.0.flow_sampling", "log_config.0.metadata"},
 						},
 					},
 				},
@@ -170,8 +125,6 @@ flow logs. Default is 'INCLUDE_ALL_METADATA'.`,
 			"private_ip_google_access": {
 				Type:     schema.TypeBool,
 				Optional: true,
-				Description: `When enabled, VMs in this subnetwork without external IP addresses can
-access Google APIs and services by using Private Google Access.`,
 			},
 			"purpose": {
 				Type:         schema.TypeString,
@@ -179,13 +132,6 @@ access Google APIs and services by using Private Google Access.`,
 				Optional:     true,
 				ForceNew:     true,
 				ValidateFunc: validation.StringInSlice([]string{"INTERNAL_HTTPS_LOAD_BALANCER", "PRIVATE", ""}, false),
-				Description: `The purpose of the resource. This field can be either PRIVATE
-or INTERNAL_HTTPS_LOAD_BALANCER. A subnetwork with purpose set to
-INTERNAL_HTTPS_LOAD_BALANCER is a user-created subnetwork that is
-reserved for Internal HTTP(S) Load Balancing. If unspecified, the
-purpose defaults to PRIVATE.
-
-If set to INTERNAL_HTTPS_LOAD_BALANCER you must also set the role.`,
 			},
 			"region": {
 				Type:             schema.TypeString,
@@ -193,69 +139,49 @@ If set to INTERNAL_HTTPS_LOAD_BALANCER you must also set the role.`,
 				Optional:         true,
 				ForceNew:         true,
 				DiffSuppressFunc: compareSelfLinkOrResourceName,
-				Description:      `URL of the GCP region for this subnetwork.`,
 			},
 			"role": {
 				Type:         schema.TypeString,
 				Optional:     true,
 				ValidateFunc: validation.StringInSlice([]string{"ACTIVE", "BACKUP", ""}, false),
-				Description: `The role of subnetwork. Currently, this field is only used when
-purpose = INTERNAL_HTTPS_LOAD_BALANCER. The value can be set to ACTIVE
-or BACKUP. An ACTIVE subnetwork is one that is currently being used
-for Internal HTTP(S) Load Balancing. A BACKUP subnetwork is one that
-is ready to be promoted to ACTIVE or is currently draining.`,
 			},
 			"secondary_ip_range": {
 				Type:       schema.TypeList,
 				Computed:   true,
 				Optional:   true,
 				ConfigMode: schema.SchemaConfigModeAttr,
-				Description: `An array of configurations for secondary IP ranges for VM instances
-contained in this subnetwork. The primary IP of such VM must belong
-to the primary ipCidrRange of the subnetwork. The alias IPs may belong
-to either primary or secondary ranges.
-This field uses attr-as-block mode to avoid breaking
-users during the 0.12 upgrade. See [the Attr-as-Block page](https://www.terraform.io/docs/configuration/attr-as-blocks.html)
-for more details.`,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"ip_cidr_range": {
 							Type:         schema.TypeString,
 							Required:     true,
 							ValidateFunc: validateIpCidrRange,
-							Description: `The range of IP addresses belonging to this subnetwork secondary
-range. Provide this property when you create the subnetwork.
-Ranges must be unique and non-overlapping with all primary and
-secondary IP ranges within a network. Only IPv4 is supported.`,
 						},
 						"range_name": {
 							Type:         schema.TypeString,
 							Required:     true,
 							ValidateFunc: validateGCPName,
-							Description: `The name associated with this subnetwork secondary range, used
-when adding an alias IP range to a VM instance. The name must
-be 1-63 characters long, and comply with RFC1035. The name
-must be unique within the subnetwork.`,
 						},
 					},
 				},
 			},
 			"creation_timestamp": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: `Creation timestamp in RFC3339 text format.`,
+				Type:     schema.TypeString,
+				Computed: true,
 			},
 			"fingerprint": {
 				Type:     schema.TypeString,
 				Computed: true,
-				Description: `Fingerprint of this resource. This field is used internally during
-updates of this resource.`,
 			},
 			"gateway_address": {
 				Type:     schema.TypeString,
 				Computed: true,
-				Description: `The gateway address for default routes to reach destination addresses
-outside this subnetwork.`,
+			},
+			"enable_flow_logs": {
+				Type:     schema.TypeBool,
+				Computed: true,
+				Optional: true,
+				Removed:  "This field is being removed in favor of log_config. If log_config is present, flow logs are enabled. Please remove this field",
 			},
 			"project": {
 				Type:     schema.TypeString,
@@ -341,12 +267,6 @@ func resourceComputeSubnetworkCreate(d *schema.ResourceData, meta interface{}) e
 	} else if v, ok := d.GetOkExists("network"); !isEmptyValue(reflect.ValueOf(networkProp)) && (ok || !reflect.DeepEqual(v, networkProp)) {
 		obj["network"] = networkProp
 	}
-	enableFlowLogsProp, err := expandComputeSubnetworkEnableFlowLogs(d.Get("enable_flow_logs"), d, config)
-	if err != nil {
-		return err
-	} else if v, ok := d.GetOkExists("enable_flow_logs"); ok || !reflect.DeepEqual(v, enableFlowLogsProp) {
-		obj["enableFlowLogs"] = enableFlowLogsProp
-	}
 	fingerprintProp, err := expandComputeSubnetworkFingerprint(d.Get("fingerprint"), d, config)
 	if err != nil {
 		return err
@@ -386,7 +306,7 @@ func resourceComputeSubnetworkCreate(d *schema.ResourceData, meta interface{}) e
 	logConfigProp, err := expandComputeSubnetworkLogConfig(d.Get("log_config"), d, config)
 	if err != nil {
 		return err
-	} else if v, ok := d.GetOkExists("log_config"); !isEmptyValue(reflect.ValueOf(logConfigProp)) && (ok || !reflect.DeepEqual(v, logConfigProp)) {
+	} else if v, ok := d.GetOkExists("log_config"); ok || !reflect.DeepEqual(v, logConfigProp) {
 		obj["logConfig"] = logConfigProp
 	}
 
@@ -472,9 +392,6 @@ func resourceComputeSubnetworkRead(d *schema.ResourceData, meta interface{}) err
 	if err := d.Set("network", flattenComputeSubnetworkNetwork(res["network"], d)); err != nil {
 		return fmt.Errorf("Error reading Subnetwork: %s", err)
 	}
-	if err := d.Set("enable_flow_logs", flattenComputeSubnetworkEnableFlowLogs(res["enableFlowLogs"], d)); err != nil {
-		return fmt.Errorf("Error reading Subnetwork: %s", err)
-	}
 	if err := d.Set("fingerprint", flattenComputeSubnetworkFingerprint(res["fingerprint"], d)); err != nil {
 		return fmt.Errorf("Error reading Subnetwork: %s", err)
 	}
@@ -547,57 +464,6 @@ func resourceComputeSubnetworkUpdate(d *schema.ResourceData, meta interface{}) e
 		}
 
 		d.SetPartial("ip_cidr_range")
-	}
-	if d.HasChange("enable_flow_logs") {
-		obj := make(map[string]interface{})
-
-		getUrl, err := replaceVars(d, config, "{{ComputeBasePath}}projects/{{project}}/regions/{{region}}/subnetworks/{{name}}")
-		if err != nil {
-			return err
-		}
-
-		project, err := getProject(d, config)
-		if err != nil {
-			return err
-		}
-		getRes, err := sendRequest(config, "GET", project, getUrl, nil)
-		if err != nil {
-			return handleNotFoundError(err, d, fmt.Sprintf("ComputeSubnetwork %q", d.Id()))
-		}
-
-		obj["fingerprint"] = getRes["fingerprint"]
-
-		enableFlowLogsProp, err := expandComputeSubnetworkEnableFlowLogs(d.Get("enable_flow_logs"), d, config)
-		if err != nil {
-			return err
-		} else if v, ok := d.GetOkExists("enable_flow_logs"); ok || !reflect.DeepEqual(v, enableFlowLogsProp) {
-			obj["enableFlowLogs"] = enableFlowLogsProp
-		}
-
-		url, err := replaceVars(d, config, "{{ComputeBasePath}}projects/{{project}}/regions/{{region}}/subnetworks/{{name}}")
-		if err != nil {
-			return err
-		}
-		res, err := sendRequestWithTimeout(config, "PATCH", project, url, obj, d.Timeout(schema.TimeoutUpdate))
-		if err != nil {
-			return fmt.Errorf("Error updating Subnetwork %q: %s", d.Id(), err)
-		}
-
-		op := &compute.Operation{}
-		err = Convert(res, op)
-		if err != nil {
-			return err
-		}
-
-		err = computeOperationWaitTime(
-			config.clientCompute, op, project, "Updating Subnetwork",
-			int(d.Timeout(schema.TimeoutUpdate).Minutes()))
-
-		if err != nil {
-			return err
-		}
-
-		d.SetPartial("enable_flow_logs")
 	}
 	if d.HasChange("role") {
 		obj := make(map[string]interface{})
@@ -758,7 +624,7 @@ func resourceComputeSubnetworkUpdate(d *schema.ResourceData, meta interface{}) e
 		logConfigProp, err := expandComputeSubnetworkLogConfig(d.Get("log_config"), d, config)
 		if err != nil {
 			return err
-		} else if v, ok := d.GetOkExists("log_config"); !isEmptyValue(reflect.ValueOf(v)) && (ok || !reflect.DeepEqual(v, logConfigProp)) {
+		} else if v, ok := d.GetOkExists("log_config"); ok || !reflect.DeepEqual(v, logConfigProp) {
 			obj["logConfig"] = logConfigProp
 		}
 
@@ -880,10 +746,6 @@ func flattenComputeSubnetworkNetwork(v interface{}, d *schema.ResourceData) inte
 	return ConvertSelfLinkToV1(v.(string))
 }
 
-func flattenComputeSubnetworkEnableFlowLogs(v interface{}, d *schema.ResourceData) interface{} {
-	return v
-}
-
 func flattenComputeSubnetworkFingerprint(v interface{}, d *schema.ResourceData) interface{} {
 	return v
 }
@@ -975,10 +837,6 @@ func expandComputeSubnetworkNetwork(v interface{}, d TerraformResourceData, conf
 	return f.RelativeLink(), nil
 }
 
-func expandComputeSubnetworkEnableFlowLogs(v interface{}, d TerraformResourceData, config *Config) (interface{}, error) {
-	return v, nil
-}
-
 func expandComputeSubnetworkFingerprint(v interface{}, d TerraformResourceData, config *Config) (interface{}, error) {
 	return v, nil
 }
@@ -1042,21 +900,27 @@ func expandComputeSubnetworkRegion(v interface{}, d TerraformResourceData, confi
 
 func expandComputeSubnetworkLogConfig(v interface{}, d TerraformResourceData, config *Config) (interface{}, error) {
 	l := v.([]interface{})
+	transformed := make(map[string]interface{})
 	if len(l) == 0 || l[0] == nil {
-		return nil, nil
+		purpose, ok := d.GetOkExists("purpose")
+
+		if ok && purpose.(string) == "INTERNAL_HTTPS_LOAD_BALANCER" {
+			// Subnetworks for L7ILB do not accept any values for logConfig
+			return nil, nil
+		}
+		// send enable = false to ensure logging is disabled if there is no config
+		transformed["enable"] = false
+		return transformed, nil
 	}
+
 	raw := l[0]
 	original := raw.(map[string]interface{})
 
-	v, ok := d.GetOkExists("enable_flow_logs")
-
-	transformed := make(map[string]interface{})
-	if !ok || v.(bool) {
-		transformed["enable"] = true
-		transformed["aggregationInterval"] = original["aggregation_interval"]
-		transformed["flowSampling"] = original["flow_sampling"]
-		transformed["metadata"] = original["metadata"]
-	}
+	// The log_config block is specified, so logging should be enabled
+	transformed["enable"] = true
+	transformed["aggregationInterval"] = original["aggregation_interval"]
+	transformed["flowSampling"] = original["flow_sampling"]
+	transformed["metadata"] = original["metadata"]
 
 	return transformed, nil
 }
