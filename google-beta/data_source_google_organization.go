c75ce8c07539b8f4adfde6ff3d1977180b1e3daa
Dana Hoffman <danahoffman@google.com>
2019-12-20 20:25:17 +0000

    Add authoritative IAM policy binding for KMS Crypto Keys
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/data_source_google_organization.go
+++ google-beta/data_source_google_organization.go
@@ -54,13 +54,9 @@ func dataSourceOrganizationRead(d *schema.ResourceData, meta interface{}) error 
 	var organization *cloudresourcemanager.Organization
 	if v, ok := d.GetOk("domain"); ok {
 		filter := fmt.Sprintf("domain=%s", v.(string))
-		var resp *cloudresourcemanager.SearchOrganizationsResponse
-		err := retryTimeDuration(func() (err error) {
-			resp, err = config.clientResourceManager.Organizations.Search(&cloudresourcemanager.SearchOrganizationsRequest{
-				Filter: filter,
-			}).Do()
-			return err
-		}, d.Timeout(schema.TimeoutRead))
+		resp, err := config.clientResourceManager.Organizations.Search(&cloudresourcemanager.SearchOrganizationsRequest{
+			Filter: filter,
+		}).Do()
 		if err != nil {
 			return fmt.Errorf("Error reading organization: %s", err)
 		}
@@ -68,18 +64,13 @@ func dataSourceOrganizationRead(d *schema.ResourceData, meta interface{}) error 
 		if len(resp.Organizations) == 0 {
 			return fmt.Errorf("Organization not found: %s", v)
 		}
-
 		if len(resp.Organizations) > 1 {
 			return fmt.Errorf("More than one matching organization found")
 		}
 
 		organization = resp.Organizations[0]
 	} else if v, ok := d.GetOk("organization"); ok {
-		var resp *cloudresourcemanager.Organization
-		err := retryTimeDuration(func() (err error) {
-			resp, err = config.clientResourceManager.Organizations.Get(canonicalOrganizationName(v.(string))).Do()
-			return err
-		}, d.Timeout(schema.TimeoutRead))
+		resp, err := config.clientResourceManager.Organizations.Get(canonicalOrganizationName(v.(string))).Do()
 		if err != nil {
 			return handleNotFoundError(err, d, fmt.Sprintf("Organization Not Found : %s", v))
 		}
