8c0de78175e025a718c0bbf5e5bf9eda2b6a05f9
Modular Magician <magic-modules@google.com>
2020-05-26 21:59:55 +0000

    New generated code for MM PR 3564.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider.go
+++ google-beta/provider.go
@@ -638,9 +638,9 @@ func Provider() terraform.ResourceProvider {
 	return provider
 }
 
-// Generated resources: 156
+// Generated resources: 157
 // Generated IAM resources: 66
-// Total generated resources: 222
+// Total generated resources: 223
 func ResourceMap() map[string]*schema.Resource {
 	resourceMap, _ := ResourceMapWithErrors()
 	return resourceMap
@@ -754,6 +754,7 @@ func ResourceMapWithErrors() (map[string]*schema.Resource, error) {
 			"google_compute_url_map":                                       resourceComputeUrlMap(),
 			"google_compute_vpn_tunnel":                                    resourceComputeVpnTunnel(),
 			"google_container_analysis_note":                               resourceContainerAnalysisNote(),
+			"google_container_analysis_occurrence":                         resourceContainerAnalysisOccurrence(),
 			"google_data_catalog_entry_group":                              resourceDataCatalogEntryGroup(),
 			"google_data_catalog_entry_group_iam_binding":                  ResourceIamBinding(DataCatalogEntryGroupIamSchema, DataCatalogEntryGroupIamUpdaterProducer, DataCatalogEntryGroupIdParseFunc),
 			"google_data_catalog_entry_group_iam_member":                   ResourceIamMember(DataCatalogEntryGroupIamSchema, DataCatalogEntryGroupIamUpdaterProducer, DataCatalogEntryGroupIdParseFunc),
