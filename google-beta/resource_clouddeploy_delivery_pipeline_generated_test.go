cdd01ee261daceeab5eb46829ef2ef099b7176df
Modular Magician <magic-modules@google.com>
2022-05-09 08:54:47 +0000

    New generated code for MM PR 5987.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_clouddeploy_delivery_pipeline_generated_test.go
+++ google-beta/resource_clouddeploy_delivery_pipeline_generated_test.go
@@ -67,17 +67,17 @@ resource "google_clouddeploy_delivery_pipeline" "primary" {
   name     = "tf-test-pipeline%{random_suffix}"
 
   annotations = {
-    my_second_annotation = "example-annotation-2"
-
     my_first_annotation = "example-annotation-1"
+
+    my_second_annotation = "example-annotation-2"
   }
 
   description = "basic description"
 
   labels = {
-    my_first_label = "example-label-1"
-
     my_second_label = "example-label-2"
+
+    my_first_label = "example-label-1"
   }
 
   project = "%{project_name}"
@@ -106,9 +106,9 @@ resource "google_clouddeploy_delivery_pipeline" "primary" {
   name     = "tf-test-pipeline%{random_suffix}"
 
   annotations = {
-    my_second_annotation = "updated-example-annotation-2"
-
     my_third_annotation = "example-annotation-3"
+
+    my_second_annotation = "updated-example-annotation-2"
   }
 
   description = "updated description"
