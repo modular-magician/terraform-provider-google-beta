3bb044880047f745357e94e0528a34366c3fb7d6
Modular Magician <magic-modules@google.com>
2022-08-30 13:58:56 +0000

    New generated code for MM PR 6477.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_compute_forwarding_rule_generated_test.go
+++ google-beta/resource_compute_forwarding_rule_generated_test.go
@@ -242,6 +242,274 @@ resource "google_compute_instance" "vm-test" {
 `, context)
 }
 
+func TestAccComputeForwardingRule_sharedVPCInternalHttpLbWithMigBackendExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProvidersOiCS,
+		CheckDestroy: testAccCheckComputeForwardingRuleDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeForwardingRule_sharedVPCInternalHttpLbWithMigBackendExample(context),
+			},
+			{
+				ResourceName:            "google_compute_forwarding_rule.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"backend_service", "network", "subnetwork", "region", "port_range", "target"},
+			},
+		},
+	})
+}
+
+func testAccComputeForwardingRule_sharedVPCInternalHttpLbWithMigBackendExample(context map[string]interface{}) string {
+	return Nprintf(`
+# Shared VPC Internal HTTP load balancer with a managed instance group backend
+
+# VPC network
+resource "google_compute_network" "ilb_network" {
+  name                    = "tf-test-l7-ilb-network%{random_suffix}"
+  provider                = google-beta
+  auto_create_subnetworks = false
+  project                 = "my-host-project-357412"
+}
+
+# proxy-only subnet
+resource "google_compute_subnetwork" "proxy_subnet" {
+  name          = "tf-test-l7-ilb-proxy-subnet%{random_suffix}"
+  provider      = google-beta
+  ip_cidr_range = "10.0.0.0/24"
+  region        = "us-central1"
+  purpose       = "INTERNAL_HTTPS_LOAD_BALANCER"
+  role          = "ACTIVE"
+  network       = google_compute_network.ilb_network.id
+  project       = "my-host-project-357412"
+}
+
+# backend subnet
+resource "google_compute_subnetwork" "ilb_subnet" {
+  name          = "tf-test-l7-ilb-subnet%{random_suffix}"
+  provider      = google-beta
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.ilb_network.id
+  project       = "my-host-project-357412"
+}
+
+# allow all access from IAP and health check ranges
+resource "google_compute_firewall" "fw_iap" {
+  project       = "my-host-project-357412"
+  name          = "tf-test-l7-ilb-fw-allow-iap-hc%{random_suffix}"
+  provider      = google-beta
+  direction     = "INGRESS"
+  network       = google_compute_network.ilb_network.id
+  source_ranges = ["130.211.0.0/22", "35.191.0.0/16", "35.235.240.0/20"]
+  allow {
+    protocol = "tcp"
+  }
+}
+
+# allow http from proxy subnet to backends
+resource "google_compute_firewall" "fw_ilb_to_backends" {
+  project       = "my-host-project-357412"
+  name          = "tf-test-l7-ilb-fw-allow-ilb-to-backends%{random_suffix}"
+  provider      = google-beta
+  direction     = "INGRESS"
+  network       = google_compute_network.ilb_network.id
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443", "8080"]
+  }
+}
+
+# forwarding rule
+resource "google_compute_forwarding_rule" "default" {
+  name                  = "tf-test-l7-ilb-forwarding-rule%{random_suffix}"
+  provider              = google-beta
+  region                = "us-central1"
+  depends_on            = [google_compute_subnetwork.proxy_subnet]
+  ip_protocol           = "TCP"
+  load_balancing_scheme = "INTERNAL_MANAGED"
+  port_range            = "80"
+  target                = google_compute_region_target_http_proxy.default.id
+  network               = google_compute_network.ilb_network.id
+  subnetwork            = google_compute_subnetwork.ilb_subnet.id
+  network_tier          = "PREMIUM"
+  project               = "my-service-project-01-358212"
+}
+
+# HTTP target proxy
+resource "google_compute_region_target_http_proxy" "default" {
+  name     = "tf-test-l7-ilb-target-http-proxy%{random_suffix}"
+  provider = google-beta
+  region   = "us-central1"
+  url_map  = google_compute_region_url_map.default.id
+  project  = "my-service-project-01-358212"
+}
+
+# URL map
+resource "google_compute_region_url_map" "default" {
+  name            = "tf-test-l7-ilb-regional-url-map%{random_suffix}"
+  provider        = google-beta
+  region          = "us-central1"
+  default_service = google_compute_region_backend_service.default.id
+  project         = "my-service-project-01-358212"
+}
+
+# regional health check
+resource "google_compute_region_health_check" "default" {
+  project  = "my-service-project-02-358212"
+  name     = "tf-test-l7-ilb-rhc%{random_suffix}"
+  provider = google-beta
+  region   = "us-central1"
+  http_health_check {
+    port_specification = "USE_SERVING_PORT"
+  }
+}
+
+# regional backend service
+resource "google_compute_region_backend_service" "default" {
+  project               = "my-service-project-02-358212"
+  name                  = "tf-test-l7-ilb-backend-service%{random_suffix}"
+  provider              = google-beta
+  region                = "us-central1"
+  protocol              = "HTTP"
+  load_balancing_scheme = "INTERNAL_MANAGED"
+  timeout_sec           = 10
+  health_checks         = [google_compute_region_health_check.default.id]
+  backend {
+    group           = google_compute_region_instance_group_manager.default.instance_group
+    balancing_mode  = "UTILIZATION"
+    capacity_scaler = 1.0
+  }
+}
+
+# health check
+resource "google_compute_health_check" "default" {
+  project            = "my-service-project-02-358212"
+  name               = "tf-test-l7-ilb-hc%{random_suffix}"
+  timeout_sec        = 1
+  check_interval_sec = 1
+  tcp_health_check {
+    port = "80"
+  }
+}
+
+# instance template
+resource "google_compute_instance_template" "default" {
+  project      = "my-service-project-02-358212"
+  name         = "tf-test-l7-ilb-mig-template%{random_suffix}"
+  provider     = google-beta
+  machine_type = "e2-small"
+  tags         = ["http-server"]
+
+  network_interface {
+    network    = google_compute_network.ilb_network.id
+    subnetwork = google_compute_subnetwork.ilb_subnet.id
+    access_config {
+      # add external ip to fetch packages
+    }
+  }
+  disk {
+    source_image = "debian-cloud/debian-10"
+    auto_delete  = true
+    boot         = true
+  }
+
+  # install nginx and serve a simple web page
+  metadata = {
+    startup-script = <<-EOF1
+      #! /bin/bash
+      set -euo pipefail
+
+      export DEBIAN_FRONTEND=noninteractive
+      apt-get update
+      apt-get install -y nginx-light jq
+
+      NAME=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/hostname")
+      IP=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip")
+      METADATA=$(curl -f -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/?recursive=True" | jq 'del(.["startup-script"])')
+
+      cat <<EOF > /var/www/html/index.html
+      <pre>
+      Name: $NAME
+      IP: $IP
+      Metadata: $METADATA
+      </pre>
+      EOF
+    EOF1
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# MIG
+resource "google_compute_region_instance_group_manager" "default" {
+  project  = "my-service-project-02-358212"
+  name     = "tf-test-l7-ilb-mig1%{random_suffix}"
+  depends_on            = [google_project_iam_binding.default]
+  provider = google-beta
+  region   = "us-central1"
+  version {
+    instance_template = google_compute_instance_template.default.id
+    name              = "primary"
+  }
+  base_instance_name = "vm"
+  target_size        = 2
+  auto_healing_policies {
+    health_check      = google_compute_health_check.default.id
+    initial_delay_sec = 300
+  }
+}
+
+data "google_project" "service_project02" {
+  project_id = "my-service-project-02-358212"
+}
+
+# IAM Role
+resource "google_project_iam_binding" "default" {
+  project = "my-host-project-357412"
+  role    = "roles/compute.networkUser"
+
+  members = [
+    "serviceAccount:${data.google_project.service_project02.number}@cloudservices.gserviceaccount.com",
+  ]
+}
+
+# test instance
+resource "google_compute_instance" "vm_test" {
+  project      = "my-service-project-02-358212"
+  name         = "tf-test-l7-ilb-test-vm%{random_suffix}"
+  depends_on   = [google_project_iam_binding.default]
+  provider     = google-beta
+  zone         = "us-central1-b"
+  machine_type = "e2-small"
+  network_interface {
+    network    = google_compute_network.ilb_network.id
+    subnetwork = google_compute_subnetwork.ilb_subnet.id
+  }
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+  lifecycle {
+    ignore_changes = [
+      metadata["ssh-keys"]
+    ]
+  }
+}
+`, context)
+}
+
 func TestAccComputeForwardingRule_internalTcpUdpLbWithMigBackendExample(t *testing.T) {
 	t.Parallel()
 
