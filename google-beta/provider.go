46888f3b769be79bb8dd6bb6d54f3520cf884a64
Modular Magician <magic-modules@google.com>
2022-11-07 18:26:57 +0000

    New generated code for MM PR 6792.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider.go
+++ google-beta/provider.go
@@ -1013,9 +1013,9 @@ func Provider() *schema.Provider {
 	return provider
 }
 
-// Generated resources: 277
+// Generated resources: 278
 // Generated IAM resources: 183
-// Total generated resources: 460
+// Total generated resources: 461
 func ResourceMap() map[string]*schema.Resource {
 	resourceMap, _ := ResourceMapWithErrors()
 	return resourceMap
@@ -1042,6 +1042,7 @@ func ResourceMapWithErrors() (map[string]*schema.Resource, error) {
 			"google_active_directory_domain":                               resourceActiveDirectoryDomain(),
 			"google_active_directory_domain_trust":                         resourceActiveDirectoryDomainTrust(),
 			"google_alloydb_cluster":                                       resourceAlloydbCluster(),
+			"google_alloydb_instance":                                      resourceAlloydbInstance(),
 			"google_api_gateway_api":                                       resourceApiGatewayApi(),
 			"google_api_gateway_api_iam_binding":                           ResourceIamBinding(ApiGatewayApiIamSchema, ApiGatewayApiIamUpdaterProducer, ApiGatewayApiIdParseFunc),
 			"google_api_gateway_api_iam_member":                            ResourceIamMember(ApiGatewayApiIamSchema, ApiGatewayApiIamUpdaterProducer, ApiGatewayApiIdParseFunc),
