70bf23b66c1a51876502b59562df6a7bebeb6412
Modular Magician <magic-modules@google.com>
2023-02-07 14:56:26 +0000

    New generated code for MM PR 7233.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_identity_platform_project_default_config.go
+++ google-beta/resource_identity_platform_project_default_config.go
@@ -84,8 +84,8 @@ func resourceIdentityPlatformProjectDefaultConfig() *schema.Resource {
 									"password_required": {
 										Type:     schema.TypeBool,
 										Optional: true,
-										Description: `Whether a password is required for email auth or not. If true, both an email and 
-password must be provided to sign in. If false, a user may sign in via either 
+										Description: `Whether a password is required for email auth or not. If true, both an email and
+password must be provided to sign in. If false, a user may sign in via either
 email/password or email link.`,
 									},
 								},
