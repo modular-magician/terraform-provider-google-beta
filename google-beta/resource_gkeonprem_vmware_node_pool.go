a084193526e172ac51ab8983ebcdf166710a864e
Modular Magician <magic-modules@google.com>
2023-05-17 17:35:36 +0000

    New generated code for MM PR 7956.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_gkeonprem_vmware_node_pool.go
+++ google-beta/resource_gkeonprem_vmware_node_pool.go
@@ -58,11 +58,13 @@ func ResourceGkeonpremVmwareNodePool() *schema.Resource {
 							Required: true,
 							Description: `The OS image to be used for each node in a node pool.
 Currently 'cos', 'ubuntu', 'ubuntu_containerd' and 'windows' are supported.`,
+							Default: "ubuntu_containerd",
 						},
 						"boot_disk_size_gb": {
 							Type:        schema.TypeInt,
 							Optional:    true,
 							Description: `VMware disk size to be used during creation.`,
+							Default:     40,
 						},
 						"cpus": {
 							Type:        schema.TypeInt,
@@ -103,7 +105,7 @@ and conflicts should be avoided.`,
 							Type:        schema.TypeInt,
 							Optional:    true,
 							Description: `The number of nodes in the node pool.`,
-							Default:     1,
+							Default:     3,
 						},
 						"taints": {
 							Type:        schema.TypeList,
