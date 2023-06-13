1d807669be696b6d3a385a87c80f4a1840fa539a
Modular Magician <magic-modules@google.com>
2023-06-13 17:57:15 +0000

    New generated code for MM PR 8143.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/gkeonprem/resource_gkeonprem_vmware_node_pool.go
+++ google-beta/services/gkeonprem/resource_gkeonprem_vmware_node_pool.go
@@ -58,12 +58,14 @@ func ResourceGkeonpremVmwareNodePool() *schema.Resource {
 					Schema: map[string]*schema.Schema{
 						"image_type": {
 							Type:     schema.TypeString,
-							Required: true,
+							Computed: true,
+							Optional: true,
 							Description: `The OS image to be used for each node in a node pool.
 Currently 'cos', 'ubuntu', 'ubuntu_containerd' and 'windows' are supported.`,
 						},
 						"boot_disk_size_gb": {
 							Type:        schema.TypeInt,
+							Computed:    true,
 							Optional:    true,
 							Description: `VMware disk size to be used during creation.`,
 						},
