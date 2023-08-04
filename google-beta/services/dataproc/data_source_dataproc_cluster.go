3a31ab338cad35a62e11689205243365e1b98c47
Modular Magician <magic-modules@google.com>
2023-08-04 21:57:10 +0000

    New generated code for MM PR 8527.
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google-beta/services/dataproc/data_source_dataproc_cluster.go
@@ -0,0 +1,51 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+package dataproc
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-google-beta/google-beta/tpgresource"
+	transport_tpg "github.com/hashicorp/terraform-provider-google-beta/google-beta/transport"
+)
+
+func DataSourceDataprocCluster() *schema.Resource {
+	// Generate datasource schema from resource
+	dsSchema := tpgresource.DatasourceSchemaFromResourceSchema(ResourceDataprocCluster().Schema)
+
+	// Set 'Required' schema elements
+	tpgresource.AddRequiredFieldsToSchema(dsSchema, "cluster_name", "region")
+
+	// Set 'Optional' schema elements
+	tpgresource.AddOptionalFieldsToSchema(dsSchema, "project")
+
+	return &schema.Resource{
+		Read:   dataSourceDataprocClusterRead,
+		Schema: dsSchema,
+	}
+}
+
+func dataSourceDataprocClusterRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*transport_tpg.Config)
+
+	project, err := tpgresource.GetProject(d, config)
+	if err != nil {
+		return err
+	}
+
+	region, err := tpgresource.GetLocation(d, config)
+	if err != nil {
+		return err
+	}
+
+	clusterName := d.Get("cluster_name").(string)
+	d.SetId(fmt.Sprintf("projects/%s/regions/%s/clusters/%s", project, region, clusterName))
+
+	err = resourceDataprocClusterRead(d, meta)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
