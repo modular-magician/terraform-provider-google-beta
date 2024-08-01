61a8be2aa3ef5fe96d39e70433d0d24ab5a4443c
Modular Magician <magic-modules@google.com>
2024-08-01 15:01:39 +0000

    New generated code for MM PR 11326.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/appengine/resource_app_engine_standard_app_version_test.go
+++ google-beta/services/appengine/resource_app_engine_standard_app_version_test.go
@@ -67,16 +67,34 @@ resource "google_project" "my_project" {
   billing_account = "%{billing_account}"
 }
 
-resource "google_app_engine_application" "app" {
-  project     = google_project.my_project.project_id
-  location_id = "us-central"
-}
-
 resource "google_project_service" "project" {
   project = google_project.my_project.project_id
   service = "appengine.googleapis.com"
 
   disable_dependent_services = false
+}
+
+resource "google_app_engine_application" "app" {
+  project     = google_project.my_project.project_id
+  location_id = "us-central"
+}
+
+data "google_app_engine_default_service_account" "default" {
+  depends_on = [
+    google_app_engine_application.app
+  ]
+}
+
+resource "google_project_iam_member" "app_eng_sa_bucket_permissions" {
+  project = google_project.my_project.project_id
+  role    = "roles/editor"
+  member  = "serviceAccount:${data.google_app_engine_default_service_account.default.email}"
+}
+
+resource "time_sleep" "wait_for_appengine_permissions_propagate" {
+  depends_on = [google_project_iam_member.app_eng_sa_bucket_permissions]
+
+  create_duration = "60s"
 }
 
 resource "google_app_engine_standard_app_version" "foo" {
@@ -123,6 +141,7 @@ resource "google_app_engine_standard_app_version" "foo" {
     }
   }
 
+  depends_on = [ time_sleep.wait_for_appengine_permissions_propagate ]
   noop_on_destroy = true
 }
 
@@ -154,16 +173,34 @@ resource "google_project" "my_project" {
   billing_account = "%{billing_account}"
 }
 
-resource "google_app_engine_application" "app" {
-  project     = google_project.my_project.project_id
-  location_id = "us-central"
-}
-
 resource "google_project_service" "project" {
   project = google_project.my_project.project_id
   service = "appengine.googleapis.com"
 
   disable_dependent_services = false
+}
+
+resource "google_app_engine_application" "app" {
+  project     = google_project.my_project.project_id
+  location_id = "us-central"
+}
+
+data "google_app_engine_default_service_account" "default" {
+  depends_on = [
+    google_app_engine_application.app
+  ]
+}
+
+resource "google_project_iam_member" "app_eng_sa_bucket_permissions" {
+  project = google_project.my_project.project_id
+  role    = "roles/editor"
+  member  = "serviceAccount:${data.google_app_engine_default_service_account.default.email}"
+}
+
+resource "time_sleep" "wait_for_appengine_permissions_propagate" {
+  depends_on = [google_project_iam_member.app_eng_sa_bucket_permissions]
+
+  create_duration = "60s"
 }
 
 resource "time_sleep" "wait_60_seconds" {
@@ -242,6 +279,7 @@ resource "google_app_engine_standard_app_version" "foo" {
     }
   }
 
+  depends_on = [ time_sleep.wait_for_appengine_permissions_propagate ]
   noop_on_destroy = true
 }
 
@@ -273,16 +311,34 @@ resource "google_project" "my_project" {
   billing_account = "%{billing_account}"
 }
 
-resource "google_app_engine_application" "app" {
-  project     = google_project.my_project.project_id
-  location_id = "us-central"
-}
-
 resource "google_project_service" "project" {
   project = google_project.my_project.project_id
   service = "appengine.googleapis.com"
 
   disable_dependent_services = false
+}
+
+resource "google_app_engine_application" "app" {
+  project     = google_project.my_project.project_id
+  location_id = "us-central"
+}
+
+data "google_app_engine_default_service_account" "default" {
+  depends_on = [
+    google_app_engine_application.app
+  ]
+}
+
+resource "google_project_iam_member" "app_eng_sa_bucket_permissions" {
+  project = google_project.my_project.project_id
+  role    = "roles/editor"
+  member  = "serviceAccount:${data.google_app_engine_default_service_account.default.email}"
+}
+
+resource "time_sleep" "wait_for_appengine_permissions_propagate" {
+  depends_on = [google_project_iam_member.app_eng_sa_bucket_permissions]
+
+  create_duration = "60s"
 }
 
 resource "google_app_engine_standard_app_version" "foo" {
@@ -319,6 +375,7 @@ resource "google_app_engine_standard_app_version" "foo" {
     max_instances = 5
   }
 
+  depends_on = [ time_sleep.wait_for_appengine_permissions_propagate ]
   noop_on_destroy = true
 }
 
