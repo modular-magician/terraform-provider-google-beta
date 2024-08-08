14a6e5e98b8debf9ff6946185a934624810ce4fc
Modular Magician <magic-modules@google.com>
2024-08-08 19:42:51 +0000

    New generated code for MM PR 11399.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/firestore/resource_firestore_document_generated_test.go
+++ google-beta/services/firestore/resource_firestore_document_generated_test.go
@@ -189,6 +189,120 @@ resource "google_firestore_document" "sub_sub_document" {
 `, context)
 }
 
+func TestAccFirestoreDocument_firestoreDocumentVariousTypesExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"org_id":        envvar.GetTestOrgFromEnv(t),
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		ExternalProviders: map[string]resource.ExternalProvider{
+			"random": {},
+			"time":   {},
+		},
+		CheckDestroy: testAccCheckFirestoreDocumentDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccFirestoreDocument_firestoreDocumentVariousTypesExample(context),
+			},
+			{
+				ResourceName:            "google_firestore_document.mydoc",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"collection", "database", "document_id"},
+			},
+		},
+	})
+}
+
+func testAccFirestoreDocument_firestoreDocumentVariousTypesExample(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_project" "project" {
+  project_id = "tf-test-project-id%{random_suffix}"
+  name       = "tf-test-project-id%{random_suffix}"
+  org_id     = "%{org_id}"
+}
+
+resource "time_sleep" "wait_60_seconds" {
+  depends_on = [google_project.project]
+
+  create_duration = "60s"
+}
+
+resource "google_project_service" "firestore" {
+  project = google_project.project.project_id
+  service = "firestore.googleapis.com"
+
+  # Needed for CI tests for permissions to propagate, should not be needed for actual usage
+  depends_on = [time_sleep.wait_60_seconds]
+}
+
+resource "google_firestore_database" "database" {
+  project     = google_project.project.project_id
+  name        = "(default)"
+  location_id = "nam5"
+  type        = "FIRESTORE_NATIVE"
+
+  depends_on = [google_project_service.firestore]
+}
+
+resource "google_firestore_document" "some_other_document" {
+  project     = google_project.project.project_id
+  database    = google_firestore_database.database.name
+  collection  = "somenewcollection"
+  document_id = "tf-test-other-doc-id%{random_suffix}"
+
+  fields = jsonencode({
+    f = { stringValue = "a field value" }
+  })
+}
+
+resource "google_firestore_document" "mydoc" {
+  project     = google_project.project.project_id
+  database    = google_firestore_database.database.name
+  collection  = "somenewcollection"
+  document_id = "tf-test-my-doc-id%{random_suffix}"
+  # See https://firebase.google.com/docs/firestore/reference/rpc/google.firestore.v1#google.firestore.v1.Value 
+  fields = jsonencode({
+    a_string_field = { stringValue = "a_string_value" }
+    an_integer_field = { integerValue = "54321" }
+    a_double_field = { doubleValue = 2.71828 }
+    a_bool_field = { booleanValue = true }
+    an_array_field = {
+        arrayValue = {
+            values: [
+                { stringValue = "an_array_entry" },
+                { stringValue = "another_array_entry" }
+            ]
+        }
+    }
+    a_map_field = {
+        mapValue = {
+            fields: {
+                key1 = { stringValue = "a_map_value_of_type_string" }
+                key2 = { integerValue = "2000" }
+            }
+        }
+    }
+    a_null_field = { nullValue = null }
+    a_latlng_field = {
+      geoPointValue = {
+        latitude = 12.0
+        longitude = 34.0
+      }
+    }
+    a_reference_field = {
+      referenceValue = resource.google_firestore_document.some_other_document.name
+    }
+  })
+}
+`, context)
+}
+
 func testAccCheckFirestoreDocumentDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
