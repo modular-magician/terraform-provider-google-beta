32d0863756ffb9d3a9085a2f94e9c51250d8fae4
Modular Magician <magic-modules@google.com>
2024-09-04 01:13:50 +0000

    New generated code for MM PR 11596.
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google-beta/services/dataproc/export_test.go
@@ -0,0 +1,8 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+package dataproc
+
+// This file is used to export private functions purely for the unit tests, which are in a
+// different package (dataproc vs dataproc_test).
+
+var FlattenSecurityConfig = flattenSecurityConfig
