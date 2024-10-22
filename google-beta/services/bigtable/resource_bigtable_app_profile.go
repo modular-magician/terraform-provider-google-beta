088e381293f5dc7d9f28c9e2e36478469f0642bf
Modular Magician <magic-modules@google.com>
2024-10-22 19:47:24 +0000

    New generated code for MM PR 12087.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/bigtable/resource_bigtable_app_profile.go
+++ google-beta/services/bigtable/resource_bigtable_app_profile.go
@@ -159,6 +159,12 @@ It is unsafe to send these requests to the same table/row/column in multiple clu
 				},
 				ConflictsWith: []string{"single_cluster_routing"},
 			},
+			"row_affinity": {
+				Type:          schema.TypeBool,
+				Optional:      true,
+				Description:   `Must be used with multi-cluster routing. If true, then this app profile will use row affinity sticky routing. With row affinity, Bigtable will route single row key requests based on the row key, rather than randomly. Instead, each row key will be assigned to a cluster by Cloud Bigtable, and will stick to that cluster. Choosing this option improves read-your-writes consistency for most requests under most circumstances, without sacrificing availability. Consistency is not guaranteed, as requests may still fail over between clusters in the event of errors or latency.`,
+				ConflictsWith: []string{"single_cluster_routing"},
+			},
 			"project": {
 				Type:     schema.TypeString,
 				Optional: true,
@@ -570,6 +576,10 @@ func flattenBigtableAppProfileMultiClusterRoutingUseAny(v interface{}, d *schema
 		}
 	}
 
+	if v.(map[string]interface{})["rowAffinity"] != nil {
+		d.Set("row_affinity", true)
+	}
+
 	return true
 }
 
@@ -645,6 +655,10 @@ func expandBigtableAppProfileMultiClusterRoutingUseAny(v interface{}, d tpgresou
 
 	for _, id := range clusterIds {
 		obj.ClusterIds = append(obj.ClusterIds, id.(string))
+	}
+
+	if _, ok := d.GetOkExists("row_affinity"); ok {
+		obj.RowAffinity = &bigtableadmin.RowAffinity{}
 	}
 
 	return obj, nil
