f03aa55a11f5ed3d4fae6859e95a496c93cccc73
Modular Magician <magic-modules@google.com>
2025-03-20 15:57:18 +0000

    New generated code for MM PR 13272.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/container/resource_container_cluster.go
+++ google-beta/services/container/resource_container_cluster.go
@@ -6939,6 +6939,7 @@ func flattenManagedPrometheusConfig(c *container.ManagedPrometheusConfig) []map[
 
 	result := make(map[string]interface{})
 	result["enabled"] = c.Enabled
+
 	autoMonitoringList := []map[string]interface{}{}
 	if c.AutoMonitoringConfig != nil && c.AutoMonitoringConfig.Scope != "" {
 		autoMonitoringMap := map[string]interface{}{
