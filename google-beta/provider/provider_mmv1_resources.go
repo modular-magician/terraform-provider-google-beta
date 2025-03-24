508d2e01c0a4fe332c556f4cdc90a0b8b2bf6229
Modular Magician <magic-modules@google.com>
2025-03-24 18:34:36 +0000

    New generated code for MM PR 13446.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider/provider_mmv1_resources.go
+++ google-beta/provider/provider_mmv1_resources.go
@@ -474,6 +474,7 @@ var generatedIAMDatasources = map[string]*schema.Resource{
 	"google_dataproc_metastore_database_iam_policy":          tpgiamresource.DataSourceIamPolicy(dataprocmetastore.DataprocMetastoreDatabaseIamSchema, dataprocmetastore.DataprocMetastoreDatabaseIamUpdaterProducer),
 	"google_dataproc_metastore_federation_iam_policy":        tpgiamresource.DataSourceIamPolicy(dataprocmetastore.DataprocMetastoreFederationIamSchema, dataprocmetastore.DataprocMetastoreFederationIamUpdaterProducer),
 	"google_dataproc_metastore_service_iam_policy":           tpgiamresource.DataSourceIamPolicy(dataprocmetastore.DataprocMetastoreServiceIamSchema, dataprocmetastore.DataprocMetastoreServiceIamUpdaterProducer),
+	"google_dataproc_metastore_table_iam_policy":             tpgiamresource.DataSourceIamPolicy(dataprocmetastore.DataprocMetastoreTableIamSchema, dataprocmetastore.DataprocMetastoreTableIamUpdaterProducer),
 	"google_dns_managed_zone_iam_policy":                     tpgiamresource.DataSourceIamPolicy(dns.DNSManagedZoneIamSchema, dns.DNSManagedZoneIamUpdaterProducer),
 	"google_gemini_repository_group_iam_policy":              tpgiamresource.DataSourceIamPolicy(gemini.GeminiRepositoryGroupIamSchema, gemini.GeminiRepositoryGroupIamUpdaterProducer),
 	"google_gke_backup_backup_plan_iam_policy":               tpgiamresource.DataSourceIamPolicy(gkebackup.GKEBackupBackupPlanIamSchema, gkebackup.GKEBackupBackupPlanIamUpdaterProducer),
@@ -552,8 +553,8 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 
 // Resources
 // Generated resources: 631
-// Generated IAM resources: 309
-// Total generated resources: 940
+// Generated IAM resources: 312
+// Total generated resources: 943
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -992,6 +993,9 @@ var generatedResources = map[string]*schema.Resource{
 	"google_dataproc_metastore_service_iam_binding":                              tpgiamresource.ResourceIamBinding(dataprocmetastore.DataprocMetastoreServiceIamSchema, dataprocmetastore.DataprocMetastoreServiceIamUpdaterProducer, dataprocmetastore.DataprocMetastoreServiceIdParseFunc),
 	"google_dataproc_metastore_service_iam_member":                               tpgiamresource.ResourceIamMember(dataprocmetastore.DataprocMetastoreServiceIamSchema, dataprocmetastore.DataprocMetastoreServiceIamUpdaterProducer, dataprocmetastore.DataprocMetastoreServiceIdParseFunc),
 	"google_dataproc_metastore_service_iam_policy":                               tpgiamresource.ResourceIamPolicy(dataprocmetastore.DataprocMetastoreServiceIamSchema, dataprocmetastore.DataprocMetastoreServiceIamUpdaterProducer, dataprocmetastore.DataprocMetastoreServiceIdParseFunc),
+	"google_dataproc_metastore_table_iam_binding":                                tpgiamresource.ResourceIamBinding(dataprocmetastore.DataprocMetastoreTableIamSchema, dataprocmetastore.DataprocMetastoreTableIamUpdaterProducer, dataprocmetastore.DataprocMetastoreTableIdParseFunc),
+	"google_dataproc_metastore_table_iam_member":                                 tpgiamresource.ResourceIamMember(dataprocmetastore.DataprocMetastoreTableIamSchema, dataprocmetastore.DataprocMetastoreTableIamUpdaterProducer, dataprocmetastore.DataprocMetastoreTableIdParseFunc),
+	"google_dataproc_metastore_table_iam_policy":                                 tpgiamresource.ResourceIamPolicy(dataprocmetastore.DataprocMetastoreTableIamSchema, dataprocmetastore.DataprocMetastoreTableIamUpdaterProducer, dataprocmetastore.DataprocMetastoreTableIdParseFunc),
 	"google_datastream_connection_profile":                                       datastream.ResourceDatastreamConnectionProfile(),
 	"google_datastream_private_connection":                                       datastream.ResourceDatastreamPrivateConnection(),
 	"google_datastream_stream":                                                   datastream.ResourceDatastreamStream(),
