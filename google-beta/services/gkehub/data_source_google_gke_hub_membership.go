423e89713e30232a79ef6756cd04ec309ffcaecb
Modular Magician <magic-modules@google.com>
2025-05-19 19:02:54 +0000

    New generated code for MM PR 13915.
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ google-beta/services/gkehub/data_source_google_gke_hub_membership.go
@@ -0,0 +1,41 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+package gkehub
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-google-beta/google-beta/tpgresource"
+	transport_tpg "github.com/hashicorp/terraform-provider-google-beta/google-beta/transport"
+)
+
+func DataSourceGoogleGkeHubMembership() *schema.Resource {
+	dsSchema := tpgresource.DatasourceSchemaFromResourceSchema(ResourceGKEHubMembership().Schema)
+	tpgresource.AddRequiredFieldsToSchema(dsSchema, "membership_id")
+	tpgresource.AddRequiredFieldsToSchema(dsSchema, "location")
+	tpgresource.AddOptionalFieldsToSchema(dsSchema, "project")
+
+	return &schema.Resource{
+		Read:   dataSourceGoogleGkeHubMembershipRead,
+		Schema: dsSchema,
+	}
+}
+
+func dataSourceGoogleGkeHubMembershipRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*transport_tpg.Config)
+
+	id, err := tpgresource.ReplaceVars(d, config, "projects/{{project}}/locations/{{location}}/memberships/{{membership_id}}")
+	if err != nil {
+		return fmt.Errorf("Error constructing id: %s", err)
+	}
+	d.SetId(id)
+
+	err = resourceGKEHubMembershipRead(d, meta)
+	if err != nil {
+		return err
+	}
+
+	// No labels or annotations for Membership datasource
+	return nil
+}
