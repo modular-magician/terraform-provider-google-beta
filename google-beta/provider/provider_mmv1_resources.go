bc2fcab943349688e56dbd27efa344511f43ded2
Modular Magician <magic-modules@google.com>
2025-10-01 19:14:41 +0000

    New generated code for MM PR 15277.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider/provider_mmv1_resources.go
+++ google-beta/provider/provider_mmv1_resources.go
@@ -616,9 +616,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 711
+// Generated resources: 712
 // Generated IAM resources: 348
-// Total generated resources: 1059
+// Total generated resources: 1060
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -1506,6 +1506,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_saas_runtime_release":                                                saasruntime.ResourceSaasRuntimeRelease(),
 	"google_saas_runtime_rollout_kind":                                           saasruntime.ResourceSaasRuntimeRolloutKind(),
 	"google_saas_runtime_saas":                                                   saasruntime.ResourceSaasRuntimeSaas(),
+	"google_saas_runtime_tenant":                                                 saasruntime.ResourceSaasRuntimeTenant(),
 	"google_saas_runtime_unit_kind":                                              saasruntime.ResourceSaasRuntimeUnitKind(),
 	"google_secret_manager_secret":                                               secretmanager.ResourceSecretManagerSecret(),
 	"google_secret_manager_secret_iam_binding":                                   tpgiamresource.ResourceIamBinding(secretmanager.SecretManagerSecretIamSchema, secretmanager.SecretManagerSecretIamUpdaterProducer, secretmanager.SecretManagerSecretIdParseFunc),
