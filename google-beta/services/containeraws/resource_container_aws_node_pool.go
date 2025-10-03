5bd6ed65dfbc54078d6b4d3266b2869a699f9b2d
Modular Magician <magic-modules@google.com>
2025-10-03 18:28:48 +0000

    New generated code for MM PR 15307.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/containeraws/resource_container_aws_node_pool.go
+++ google-beta/services/containeraws/resource_container_aws_node_pool.go
@@ -61,15 +61,15 @@ func ResourceContainerAwsNodePool() *schema.Resource {
 			Version: 1,
 			SchemaFunc: func() map[string]*schema.Schema {
 				return map[string]*schema.Schema{
-					"location": {
-						Type:              schema.TypeString,
-						RequiredForImport: true,
-						Description:       "The location for the resource",
-					},
 					"cluster": {
 						Type:              schema.TypeString,
 						RequiredForImport: true,
 						Description:       "The awsCluster for the resource",
+					},
+					"location": {
+						Type:              schema.TypeString,
+						RequiredForImport: true,
+						Description:       "The location for the resource",
 					},
 					"project": {
 						Type:              schema.TypeString,
