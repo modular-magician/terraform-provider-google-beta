4aadccef5d7b398bd1e5ac1067ff3e66a1e54386
Modular Magician <magic-modules@google.com>
2025-10-13 19:52:59 +0000

    New generated code for MM PR 15368.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/containerazure/resource_container_azure_node_pool.go
+++ google-beta/services/containerazure/resource_container_azure_node_pool.go
@@ -60,6 +60,16 @@ func ResourceContainerAzureNodePool() *schema.Resource {
 			Version: 1,
 			SchemaFunc: func() map[string]*schema.Schema {
 				return map[string]*schema.Schema{
+					"cluster": {
+						Type:              schema.TypeString,
+						RequiredForImport: true,
+						Description:       "The azureCluster for the resource",
+					},
+					"name": {
+						Type:              schema.TypeString,
+						RequiredForImport: true,
+						Description:       "The name of this resource.",
+					},
 					"project": {
 						Type:              schema.TypeString,
 						OptionalForImport: true,
@@ -69,16 +79,6 @@ func ResourceContainerAzureNodePool() *schema.Resource {
 						Type:              schema.TypeString,
 						RequiredForImport: true,
 						Description:       "The location for the resource",
-					},
-					"name": {
-						Type:              schema.TypeString,
-						RequiredForImport: true,
-						Description:       "The name of this resource.",
-					},
-					"cluster": {
-						Type:              schema.TypeString,
-						RequiredForImport: true,
-						Description:       "The azureCluster for the resource",
 					},
 				}
 			},
--- google-beta/services/dataplex/resource_dataplex_asset.go
+++ google-beta/services/dataplex/resource_dataplex_asset.go
@@ -60,30 +60,30 @@ func ResourceDataplexAsset() *schema.Resource {
 			Version: 1,
 			SchemaFunc: func() map[string]*schema.Schema {
 				return map[string]*schema.Schema{
-					"lake": {
+					"location": {
 						Type:              schema.TypeString,
 						RequiredForImport: true,
-						Description:       "The lake for the resource",
+						Description:       "The location for the resource",
 					},
 					"dataplex_zone": {
 						Type:              schema.TypeString,
 						RequiredForImport: true,
 						Description:       "The zone for the resource",
 					},
-					"location": {
+					"lake": {
 						Type:              schema.TypeString,
 						RequiredForImport: true,
-						Description:       "The location for the resource",
-					},
-					"name": {
-						Type:              schema.TypeString,
-						RequiredForImport: true,
-						Description:       "The name of the asset.",
+						Description:       "The lake for the resource",
 					},
 					"project": {
 						Type:              schema.TypeString,
 						OptionalForImport: true,
 						Description:       "The project for the resource",
+					},
+					"name": {
+						Type:              schema.TypeString,
+						RequiredForImport: true,
+						Description:       "The name of the asset.",
 					},
 				}
 			},
