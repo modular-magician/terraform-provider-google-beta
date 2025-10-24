4be15c6e4578441799d0ee5d64a401c601cd2283
Modular Magician <magic-modules@google.com>
2025-10-24 19:04:46 +0000

    New generated code for MM PR 15501.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/apigee/resource_apigee_developer_app_update_test.go
+++ google-beta/services/apigee/resource_apigee_developer_app_update_test.go
@@ -28,6 +28,8 @@ import (
 func TestAccApigeeDeveloperApp_apigeeDeveloperAppUpdateTest(t *testing.T) {
 	t.Parallel()
 
+	resourceName := "google_apigee_developer_app.apigee_developer_app"
+
 	context := map[string]interface{}{
 		"billing_account": envvar.GetTestBillingAccountFromEnv(t),
 		"org_id":          envvar.GetTestOrgFromEnv(t),
@@ -44,18 +46,24 @@ func TestAccApigeeDeveloperApp_apigeeDeveloperAppUpdateTest(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccApigeeDeveloperApp_apigeeDeveloperAppBasicTest(context),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("google_apigee_developer_app.apigee_developer_app", "credentials.0"),
+				),
 			},
 			{
-				ResourceName:            "google_apigee_developer_app.apigee_developer_app",
+				ResourceName:            resourceName,
 				ImportState:             true,
 				ImportStateVerify:       true,
 				ImportStateVerifyIgnore: []string{"org_id"},
 			},
 			{
 				Config: testAccApigeeDeveloperApp_apigeeDeveloperAppUpdateTest(context),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("google_apigee_developer_app.apigee_developer_app", "credentials.0"),
+				),
 			},
 			{
-				ResourceName:            "google_apigee_developer_app.apigee_developer_app",
+				ResourceName:            resourceName,
 				ImportState:             true,
 				ImportStateVerify:       true,
 				ImportStateVerifyIgnore: []string{"org_id"},
