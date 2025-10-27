1525f40afce3bf82e096b1747a79bcfada24ae58
Modular Magician <magic-modules@google.com>
2025-10-27 17:29:52 +0000

    New generated code for MM PR 15427.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider/provider_mmv1_resources.go
+++ google-beta/provider/provider_mmv1_resources.go
@@ -43,6 +43,7 @@ import (
 	"github.com/hashicorp/terraform-provider-google-beta/google-beta/services/binaryauthorization"
 	"github.com/hashicorp/terraform-provider-google-beta/google-beta/services/blockchainnodeengine"
 	"github.com/hashicorp/terraform-provider-google-beta/google-beta/services/certificatemanager"
+	"github.com/hashicorp/terraform-provider-google-beta/google-beta/services/ces"
 	"github.com/hashicorp/terraform-provider-google-beta/google-beta/services/chronicle"
 	"github.com/hashicorp/terraform-provider-google-beta/google-beta/services/cloudasset"
 	"github.com/hashicorp/terraform-provider-google-beta/google-beta/services/cloudbuild"
@@ -621,9 +622,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 727
+// Generated resources: 728
 // Generated IAM resources: 348
-// Total generated resources: 1075
+// Total generated resources: 1076
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -789,6 +790,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_certificate_manager_certificate_map_entry":                           certificatemanager.ResourceCertificateManagerCertificateMapEntry(),
 	"google_certificate_manager_dns_authorization":                               certificatemanager.ResourceCertificateManagerDnsAuthorization(),
 	"google_certificate_manager_trust_config":                                    certificatemanager.ResourceCertificateManagerTrustConfig(),
+	"google_ces_app":                                                             ces.ResourceCESApp(),
 	"google_chronicle_data_access_label":                                         chronicle.ResourceChronicleDataAccessLabel(),
 	"google_chronicle_data_access_scope":                                         chronicle.ResourceChronicleDataAccessScope(),
 	"google_chronicle_reference_list":                                            chronicle.ResourceChronicleReferenceList(),
@@ -1897,6 +1899,7 @@ func UseGeneratedProducts() {
 	var _ = binaryauthorization.ProductName
 	var _ = blockchainnodeengine.ProductName
 	var _ = certificatemanager.ProductName
+	var _ = ces.ProductName
 	var _ = chronicle.ProductName
 	var _ = cloudasset.ProductName
 	var _ = cloudbuild.ProductName
