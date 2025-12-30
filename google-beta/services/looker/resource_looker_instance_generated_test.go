7371d5c2852adf0c3786545f96fb50359ce0ed49
Modular Magician <magic-modules@google.com>
2025-12-30 18:18:06 +0000

    Old generated code for MM PR 16047.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/looker/resource_looker_instance_generated_test.go
+++ google-beta/services/looker/resource_looker_instance_generated_test.go
@@ -383,24 +383,8 @@ resource "google_looker_instance" "looker-instance" {
   }
   psc_config {
     allowed_vpcs = ["projects/test-project/global/networks/test"]
-    
-    # First Service Attachment
-    # service_attachments {
-    #   local_fqdn                    = "www.example-one.com"
-    #   target_service_attachment_uri = "projects/my-project/regions/us-east1/serviceAttachments/sa-1"
-    # }
-
-    # Second Service Attachment
-    # service_attachments {
-    #   local_fqdn                    = "api.internal-partner.com"
-    #   target_service_attachment_uri = "projects/partner-project/regions/us-central1/serviceAttachments/sa-gateway"
-    # }
-
-    # Third Service Attachment
-    # service_attachments {
-    #   local_fqdn                    = "git.internal-repo.com"
-    #   target_service_attachment_uri = "projects/devops-project/regions/us-west1/serviceAttachments/gitlab-sa"
-    # }
+    # update only
+    # service_attachments = [{local_fqdn: "www.local-fqdn.com" target_service_attachment_uri: "projects/my-project/regions/us-east1/serviceAttachments/sa"}]
   }
 }
 `, context)
