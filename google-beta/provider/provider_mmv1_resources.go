d417a259b409ad140382f2d4b7f36ed18a3d5853
Modular Magician <magic-modules@google.com>
2026-01-19 18:40:39 +0000

    New generated code for MM PR 16164.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider/provider_mmv1_resources.go
+++ google-beta/provider/provider_mmv1_resources.go
@@ -633,9 +633,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 769
+// Generated resources: 770
 // Generated IAM resources: 351
-// Total generated resources: 1120
+// Total generated resources: 1121
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -1200,6 +1200,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_dns_policy":                                                          dns.ResourceDNSPolicy(),
 	"google_dns_response_policy":                                                 dns.ResourceDNSResponsePolicy(),
 	"google_dns_response_policy_rule":                                            dns.ResourceDNSResponsePolicyRule(),
+	"google_document_ai_evaluation":                                              documentai.ResourceDocumentAIEvaluation(),
 	"google_document_ai_processor":                                               documentai.ResourceDocumentAIProcessor(),
 	"google_document_ai_processor_default_version":                               documentai.ResourceDocumentAIProcessorDefaultVersion(),
 	"google_document_ai_warehouse_document_schema":                               documentaiwarehouse.ResourceDocumentAIWarehouseDocumentSchema(),
