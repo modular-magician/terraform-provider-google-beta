7f037fba79e16e5a7de2569dbfc810807924fe6a
Modular Magician <magic-modules@google.com>
2026-04-24 15:05:16 +0000

    New generated code for MM PR 17073.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/transport/config.go
+++ google-beta/transport/config.go
@@ -318,6 +318,7 @@ type Config struct {
 	DataCatalogBasePath              string
 	DataformBasePath                 string
 	DataFusionBasePath               string
+	DataLineageBasePath              string
 	DataLossPreventionBasePath       string
 	DataPipelineBasePath             string
 	DataplexBasePath                 string
@@ -512,6 +513,7 @@ const DatabaseMigrationServiceBasePathKey = "DatabaseMigrationService"
 const DataCatalogBasePathKey = "DataCatalog"
 const DataformBasePathKey = "Dataform"
 const DataFusionBasePathKey = "DataFusion"
+const DataLineageBasePathKey = "DataLineage"
 const DataLossPreventionBasePathKey = "DataLossPrevention"
 const DataPipelineBasePathKey = "DataPipeline"
 const DataplexBasePathKey = "Dataplex"
@@ -693,6 +695,7 @@ var DefaultBasePaths = map[string]string{
 	DataCatalogBasePathKey:              "https://datacatalog.googleapis.com/v1beta1/",
 	DataformBasePathKey:                 "https://dataform.googleapis.com/v1beta1/",
 	DataFusionBasePathKey:               "https://datafusion.googleapis.com/v1beta1/",
+	DataLineageBasePathKey:              "https://datalineage.googleapis.com/v1/",
 	DataLossPreventionBasePathKey:       "https://dlp.googleapis.com/v2/",
 	DataPipelineBasePathKey:             "https://datapipelines.googleapis.com/v1/",
 	DataplexBasePathKey:                 "https://dataplex.googleapis.com/v1/",
@@ -883,6 +886,7 @@ var DefaultRepStatus = map[string]bool{
 	DataCatalogBasePathKey:              false,
 	DataformBasePathKey:                 false,
 	DataFusionBasePathKey:               false,
+	DataLineageBasePathKey:              false,
 	DataLossPreventionBasePathKey:       false,
 	DataPipelineBasePathKey:             false,
 	DataplexBasePathKey:                 false,
@@ -2194,6 +2198,7 @@ func ConfigureBasePaths(c *Config) {
 	c.DataCatalogBasePath = DefaultBasePaths[DataCatalogBasePathKey]
 	c.DataformBasePath = DefaultBasePaths[DataformBasePathKey]
 	c.DataFusionBasePath = DefaultBasePaths[DataFusionBasePathKey]
+	c.DataLineageBasePath = DefaultBasePaths[DataLineageBasePathKey]
 	c.DataLossPreventionBasePath = DefaultBasePaths[DataLossPreventionBasePathKey]
 	c.DataPipelineBasePath = DefaultBasePaths[DataPipelineBasePathKey]
 	c.DataplexBasePath = DefaultBasePaths[DataplexBasePathKey]
