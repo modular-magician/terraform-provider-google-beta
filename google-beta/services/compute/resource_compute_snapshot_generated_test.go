85cbb69cfbd7311dd7c9e986d2b4b6424779b19b
Modular Magician <magic-modules@google.com>
2026-04-30 16:45:09 +0000

    New generated code for MM PR 17318.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/compute/resource_compute_snapshot_generated_test.go
+++ google-beta/services/compute/resource_compute_snapshot_generated_test.go
@@ -73,7 +73,7 @@ func TestAccComputeSnapshot_snapshotBasicExample(t *testing.T) {
 				ResourceName:            "google_compute_snapshot.snapshot",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"guest_flush", "labels", "snapshot_encryption_key.0.raw_key", "snapshot_encryption_key.0.rsa_encrypted_key", "source_disk", "source_disk_encryption_key", "source_instant_snapshot", "terraform_labels", "zone"},
+				ImportStateVerifyIgnore: []string{"guest_flush", "labels", "params", "snapshot_encryption_key.0.raw_key", "snapshot_encryption_key.0.rsa_encrypted_key", "source_disk", "source_disk_encryption_key", "source_instant_snapshot", "terraform_labels", "zone"},
 			},
 			{
 				ResourceName:       "google_compute_snapshot.snapshot",
@@ -135,7 +135,7 @@ func TestAccComputeSnapshot_snapshotBasic2Example(t *testing.T) {
 				ResourceName:            "google_compute_snapshot.snapshot",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"guest_flush", "labels", "snapshot_encryption_key.0.raw_key", "snapshot_encryption_key.0.rsa_encrypted_key", "source_disk", "source_disk_encryption_key", "source_instant_snapshot", "terraform_labels", "zone"},
+				ImportStateVerifyIgnore: []string{"guest_flush", "labels", "params", "snapshot_encryption_key.0.raw_key", "snapshot_encryption_key.0.rsa_encrypted_key", "source_disk", "source_disk_encryption_key", "source_instant_snapshot", "terraform_labels", "zone"},
 			},
 			{
 				ResourceName:       "google_compute_snapshot.snapshot",
@@ -202,7 +202,7 @@ func TestAccComputeSnapshot_snapshotBasicSourceInstantSnapshotExample(t *testing
 				ResourceName:            "google_compute_snapshot.snapshot",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"guest_flush", "labels", "snapshot_encryption_key.0.raw_key", "snapshot_encryption_key.0.rsa_encrypted_key", "source_disk", "source_disk_encryption_key", "source_instant_snapshot", "terraform_labels", "zone"},
+				ImportStateVerifyIgnore: []string{"guest_flush", "labels", "params", "snapshot_encryption_key.0.raw_key", "snapshot_encryption_key.0.rsa_encrypted_key", "source_disk", "source_disk_encryption_key", "source_instant_snapshot", "terraform_labels", "zone"},
 			},
 			{
 				ResourceName:       "google_compute_snapshot.snapshot",
@@ -272,7 +272,7 @@ func TestAccComputeSnapshot_snapshotChainnameExample(t *testing.T) {
 				ResourceName:            "google_compute_snapshot.snapshot",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"guest_flush", "labels", "snapshot_encryption_key.0.raw_key", "snapshot_encryption_key.0.rsa_encrypted_key", "source_disk", "source_disk_encryption_key", "source_instant_snapshot", "terraform_labels", "zone"},
+				ImportStateVerifyIgnore: []string{"guest_flush", "labels", "params", "snapshot_encryption_key.0.raw_key", "snapshot_encryption_key.0.rsa_encrypted_key", "source_disk", "source_disk_encryption_key", "source_instant_snapshot", "terraform_labels", "zone"},
 			},
 			{
 				ResourceName:       "google_compute_snapshot.snapshot",
