c61b10f1a5cbaf4d3f0f87ed02f3b45859d0a970
Modular Magician <magic-modules@google.com>
2026-05-15 18:32:23 +0000

    New generated code for MM PR 17584.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/acctest/tgc_utils.go
+++ google-beta/acctest/tgc_utils.go
@@ -55,6 +55,7 @@ type TgcMetadataPayload struct {
 	ResourceMetadata map[string]ResourceMetadata `json:"resource_metadata"`
 	PrimaryResource  string                      `json:"primary_resource"`
 	CaiReadTime      time.Time                   `json:"cai_read_time"`
+	RawStateFile     string                      `json:"raw_state_file,omitempty"`
 }
 
 // PROJECT_NUMBER instead of PROJECT_ID is in the CAI asset names for the resources in those services
@@ -120,6 +121,16 @@ func CollectAllTgcMetadata(tgcPayload TgcMetadataPayload) resource.TestCheckFunc
 			if rState == nil || rState.Primary == nil {
 				log.Printf("[DEBUG]TGC Terraform error: resource state unavailable for %s, skipping", address)
 				continue
+			}
+
+			if metadata.ImportMetadata.Id != "" {
+				stateBytes, err := json.Marshal(s)
+				if err == nil {
+					tgcPayload.RawStateFile = string(stateBytes)
+					log.Printf("[DEBUG] test_step_number=%d TGC captured full state for primary resource %s", tgcPayload.StepNumber, address)
+				} else {
+					log.Printf("[DEBUG] test_step_number=%d TGC could not marshal full state: %v", tgcPayload.StepNumber, err)
+				}
 			}
 
 			// Technically this will always be populated before this point but keeping it here for now so
