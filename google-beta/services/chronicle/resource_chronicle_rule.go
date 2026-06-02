822eb9e60df008cd08c98e1de0b9e35b6ef1fc8f
Modular Magician <magic-modules@google.com>
2026-06-02 00:12:06 +0000

    New generated code for MM PR 17788.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/chronicle/resource_chronicle_rule.go
+++ google-beta/services/chronicle/resource_chronicle_rule.go
@@ -55,6 +55,16 @@ import (
 	"google.golang.org/api/googleapi"
 )
 
+// chronicleRuleTextDiffSuppress compares two Rule texts, ignoring differences
+// between whitespaces and newlines (including trailing newline) that are removed
+// or added by the server.
+func chronicleRuleTextDiffSuppress(_, old, new string, _ *schema.ResourceData) bool {
+	r := strings.NewReplacer("\n", "")
+	normalizedOld := r.Replace(old)
+	normalizedNew := r.Replace(new)
+	return normalizedOld == normalizedNew
+}
+
 var (
 	_ = bytes.Clone
 	_ = context.WithCancel
@@ -181,8 +191,9 @@ The scope should be in the format:
 "projects/{project}/locations/{location}/instances/{instance}/dataAccessScopes/{scope}".`,
 			},
 			"text": {
-				Type:     schema.TypeString,
-				Optional: true,
+				Type:             schema.TypeString,
+				Optional:         true,
+				DiffSuppressFunc: chronicleRuleTextDiffSuppress,
 				Description: `The YARA-L content of the rule.
 Populated in FULL view.`,
 			},
