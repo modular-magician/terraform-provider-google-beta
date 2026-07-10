e101c8f2bc0f8e54f6587afdb1b827542a7ed303
Modular Magician <magic-modules@google.com>
2026-07-10 23:08:36 +0000

    New generated code for MM PR 17879.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/compute/resource_compute_network_edge_security_service_generated_test.go
+++ google-beta/services/compute/resource_compute_network_edge_security_service_generated_test.go
@@ -59,6 +59,7 @@ func TestAccComputeNetworkEdgeSecurityService_computeNetworkEdgeSecurityServiceB
 
 	context := map[string]interface{}{
 		"project_id":    envvar.GetTestProjectFromEnv(),
+		"region":        "us-east1",
 		"resource_name": "tf-test-my-edge-security-service" + randomSuffix,
 		"random_suffix": randomSuffix,
 	}
@@ -93,7 +94,7 @@ resource "google_compute_network_edge_security_service" "default" {
   provider     = google-beta  
 
   name         = "%{resource_name}"
-  region       = "us-east1"
+  region       = "%{region}"
   description  = "My basic resource"
 }
 `, context)
