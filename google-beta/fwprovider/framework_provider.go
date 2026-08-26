722c585a22383b241fbcdd39e8fbc10f32772663
Modular Magician <magic-modules@google.com>
2026-08-26 04:54:50 +0000

    New generated code for MM PR 18793.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/fwprovider/framework_provider.go
+++ google-beta/fwprovider/framework_provider.go
@@ -1265,6 +1265,12 @@ func (p *FrameworkProvider) Schema(_ context.Context, _ provider.SchemaRequest, 
 					transport_tpg.CustomEndpointValidator(),
 				},
 			},
+			"storage_ftp_custom_endpoint": &schema.StringAttribute{
+				Optional: true,
+				Validators: []validator.String{
+					transport_tpg.CustomEndpointValidator(),
+				},
+			},
 			"storage_insights_custom_endpoint": &schema.StringAttribute{
 				Optional: true,
 				Validators: []validator.String{
