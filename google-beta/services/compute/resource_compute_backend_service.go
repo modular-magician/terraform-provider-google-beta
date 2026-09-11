6dd42ddebd1f8a16f9280c9bda94247497a97403
Modular Magician <magic-modules@google.com>
2026-09-11 20:36:40 +0000

    New generated code for MM PR 18971.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/compute/resource_compute_backend_service.go
+++ google-beta/services/compute/resource_compute_backend_service.go
@@ -5063,7 +5063,7 @@ func expandComputeBackendServiceCdnPolicy(v interface{}, d tpgresource.Terraform
 	transformedDefaultTtl, err := expandComputeBackendServiceCdnPolicyDefaultTtl(original["default_ttl"], d, config)
 	if err != nil {
 		return nil, err
-	} else if val := reflect.ValueOf(transformedDefaultTtl); val.IsValid() && !tpgresource.IsEmptyValue(val) {
+	} else {
 		transformed["defaultTtl"] = transformedDefaultTtl
 	}
 
@@ -5077,7 +5077,7 @@ func expandComputeBackendServiceCdnPolicy(v interface{}, d tpgresource.Terraform
 	transformedClientTtl, err := expandComputeBackendServiceCdnPolicyClientTtl(original["client_ttl"], d, config)
 	if err != nil {
 		return nil, err
-	} else if val := reflect.ValueOf(transformedClientTtl); val.IsValid() && !tpgresource.IsEmptyValue(val) {
+	} else {
 		transformed["clientTtl"] = transformedClientTtl
 	}
 
--- google-beta/services/compute/resource_compute_region_backend_service.go
+++ google-beta/services/compute/resource_compute_region_backend_service.go
@@ -4789,7 +4789,7 @@ func expandComputeRegionBackendServiceCdnPolicy(v interface{}, d tpgresource.Ter
 	transformedDefaultTtl, err := expandComputeRegionBackendServiceCdnPolicyDefaultTtl(original["default_ttl"], d, config)
 	if err != nil {
 		return nil, err
-	} else if val := reflect.ValueOf(transformedDefaultTtl); val.IsValid() && !tpgresource.IsEmptyValue(val) {
+	} else {
 		transformed["defaultTtl"] = transformedDefaultTtl
 	}
 
@@ -4803,7 +4803,7 @@ func expandComputeRegionBackendServiceCdnPolicy(v interface{}, d tpgresource.Ter
 	transformedClientTtl, err := expandComputeRegionBackendServiceCdnPolicyClientTtl(original["client_ttl"], d, config)
 	if err != nil {
 		return nil, err
-	} else if val := reflect.ValueOf(transformedClientTtl); val.IsValid() && !tpgresource.IsEmptyValue(val) {
+	} else {
 		transformed["clientTtl"] = transformedClientTtl
 	}
 
