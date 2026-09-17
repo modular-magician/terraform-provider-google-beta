bd7e9f6a5065b32327cf4da7696253334c607997
Modular Magician <magic-modules@google.com>
2026-09-17 17:17:16 +0000

    New generated code for MM PR 19065.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/bigtable/resource_bigtable_instance_iam_test.go
+++ google-beta/services/bigtable/resource_bigtable_instance_iam_test.go
@@ -36,9 +36,9 @@ func TestAccBigtableInstanceIamBinding(t *testing.T) {
 	randomString := acctest.RandString(t, 10)
 
 	context := map[string]interface{}{
-		"instance": "tf-bigtable-iam-" + randomString,
-		"cluster":  "c-" + randomString,
-		"account":  "tf-bigtable-iam-" + randomString,
+		"instance": "tf-test-bt-iam-" + randomString,
+		"cluster":  "tf-test-c-" + randomString,
+		"account":  "tf-test-bt-iam-" + randomString,
 		"role":     "roles/bigtable.user",
 	}
 
@@ -85,9 +85,9 @@ func TestAccBigtableInstanceIamMember(t *testing.T) {
 	randomString := acctest.RandString(t, 10)
 
 	context := map[string]interface{}{
-		"instance": "tf-bigtable-iam-" + randomString,
-		"cluster":  "c-" + randomString,
-		"account":  "tf-bigtable-iam-" + randomString,
+		"instance": "tf-test-bt-iam-" + randomString,
+		"cluster":  "tf-test-c-" + randomString,
+		"account":  "tf-test-bt-iam-" + randomString,
 		"role":     "roles/bigtable.user",
 	}
 
@@ -129,9 +129,9 @@ func TestAccBigtableInstanceIamPolicy(t *testing.T) {
 	randomString := acctest.RandString(t, 10)
 
 	context := map[string]interface{}{
-		"instance": "tf-bigtable-iam-" + randomString,
-		"cluster":  "c-" + randomString,
-		"account":  "tf-bigtable-iam-" + randomString,
+		"instance": "tf-test-bt-iam-" + randomString,
+		"cluster":  "tf-test-c-" + randomString,
+		"account":  "tf-test-bt-iam-" + randomString,
 		"role":     "roles/bigtable.user",
 	}
 
@@ -165,9 +165,9 @@ func TestAccBigtableInstanceIamBinding_withCondition(t *testing.T) {
 	randomString := acctest.RandString(t, 10)
 	conditionExpression := "request.time < timestamp(\"2020-01-01T00:00:00Z\")"
 	context := map[string]interface{}{
-		"instance":        "tf-bigtable-iam-" + randomString,
-		"cluster":         "c-" + randomString,
-		"account":         "tf-bigtable-iam-" + randomString,
+		"instance":        "tf-test-bt-iam-" + randomString,
+		"cluster":         "tf-test-c-" + randomString,
+		"account":         "tf-test-bt-iam-" + randomString,
 		"role":            "roles/bigtable.user",
 		"condition_title": "expires_after_2019_12_31",
 		"condition_expr":  strconv.Quote(conditionExpression),
@@ -213,9 +213,9 @@ func TestAccBigtableInstanceIamMember_withCondition(t *testing.T) {
 
 	conditionExpression := "request.time < timestamp(\"2020-01-01T00:00:00Z\")"
 	context := map[string]interface{}{
-		"instance":        "tf-bigtable-iam-" + randomString,
-		"cluster":         "c-" + randomString,
-		"account":         "tf-bigtable-iam-" + randomString,
+		"instance":        "tf-test-bt-iam-" + randomString,
+		"cluster":         "tf-test-c-" + randomString,
+		"account":         "tf-test-bt-iam-" + randomString,
 		"role":            "roles/bigtable.user",
 		"condition_title": "expires_after_2019_12_31",
 		"condition_expr":  strconv.Quote(conditionExpression),
@@ -262,9 +262,9 @@ func TestAccBigtableInstanceIamPolicy_withCondition(t *testing.T) {
 	randomString := acctest.RandString(t, 10)
 	conditionExpression := "request.time < timestamp(\"2020-01-01T00:00:00Z\")"
 	context := map[string]interface{}{
-		"instance":        "tf-bigtable-iam-" + randomString,
-		"cluster":         "c-" + randomString,
-		"account":         "tf-bigtable-iam-" + randomString,
+		"instance":        "tf-test-bt-iam-" + randomString,
+		"cluster":         "tf-test-c-" + randomString,
+		"account":         "tf-test-bt-iam-" + randomString,
 		"role":            "roles/bigtable.user",
 		"condition_title": "expires_after_2019_12_31",
 		"condition_expr":  strconv.Quote(conditionExpression),
--- google-beta/services/bigtable/resource_bigtable_table_iam_test.go
+++ google-beta/services/bigtable/resource_bigtable_table_iam_test.go
@@ -32,9 +32,9 @@ func TestAccBigtableTableIamBinding(t *testing.T) {
 	acctest.SkipIfVcr(t)
 	t.Parallel()
 
-	instance := "tf-bigtable-iam-" + acctest.RandString(t, 10)
-	cluster := "c-" + acctest.RandString(t, 10)
-	account := "tf-bigtable-iam-" + acctest.RandString(t, 10)
+	instance := "tf-test-bt-iam-" + acctest.RandString(t, 10)
+	cluster := "tf-test-c-" + acctest.RandString(t, 10)
+	account := "tf-test-bt-iam-" + acctest.RandString(t, 10)
 	role := "roles/bigtable.user"
 
 	importId := fmt.Sprintf("projects/%s/instances/%s/tables/%s %s",
@@ -77,9 +77,9 @@ func TestAccBigtableTableIamMember(t *testing.T) {
 	acctest.SkipIfVcr(t)
 	t.Parallel()
 
-	instance := "tf-bigtable-iam-" + acctest.RandString(t, 10)
-	cluster := "c-" + acctest.RandString(t, 10)
-	account := "tf-bigtable-iam-" + acctest.RandString(t, 10)
+	instance := "tf-test-bt-iam-" + acctest.RandString(t, 10)
+	cluster := "tf-test-c-" + acctest.RandString(t, 10)
+	account := "tf-test-bt-iam-" + acctest.RandString(t, 10)
 	role := "roles/bigtable.user"
 
 	importId := fmt.Sprintf("projects/%s/instances/%s/tables/%s %s serviceAccount:%s",
@@ -118,9 +118,9 @@ func TestAccBigtableTableIamPolicy(t *testing.T) {
 	acctest.SkipIfVcr(t)
 	t.Parallel()
 
-	instance := "tf-bigtable-iam-" + acctest.RandString(t, 10)
-	cluster := "c-" + acctest.RandString(t, 10)
-	account := "tf-bigtable-iam-" + acctest.RandString(t, 10)
+	instance := "tf-test-bt-iam-" + acctest.RandString(t, 10)
+	cluster := "tf-test-c-" + acctest.RandString(t, 10)
+	account := "tf-test-bt-iam-" + acctest.RandString(t, 10)
 	role := "roles/bigtable.user"
 
 	importId := fmt.Sprintf("projects/%s/instances/%s/tables/%s",
