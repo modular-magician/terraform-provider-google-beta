a0811e05443c9cc81c562553f29947aab3050051
Modular Magician <magic-modules@google.com>
2026-09-21 19:59:34 +0000

    New generated code for MM PR 19111.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/dataproc/resource_dataproc_session_template_sweeper.go
+++ google-beta/services/dataproc/resource_dataproc_session_template_sweeper.go
@@ -57,12 +57,14 @@ func listAndActionDataprocSessionTemplate(action sweeper.ResourceAction) error {
 	var configs []*tpgresource.ResourceDataMock
 	t := &testing.T{}
 	billingId := envvar.GetTestBillingAccountFromEnv(t)
-	// Default single config
-	intermediateValues := []map[string]string{
-		{
-			"region": "us-central1",
-		},
-	}
+	// Build URL substitution maps individually to ensure proper formatting
+	intermediateValues := make([]map[string]string, 3)
+	intermediateValues[0] = map[string]string{}
+	intermediateValues[0]["region"] = "us-central1"
+	intermediateValues[1] = map[string]string{}
+	intermediateValues[1]["region"] = "us-west1"
+	intermediateValues[2] = map[string]string{}
+	intermediateValues[2]["region"] = "us-east1"
 
 	// Create configs from intermediate values
 	for _, values := range intermediateValues {
